66b7682174dd0cdf467aa81b40154f2ea9e0d231
k724k <k724k@naver.com>
2022-10-25 17:13:18 +0900

    init: [이연경] vuetify 설치 후 프로젝트 생성

--- p4th/YeongyeongLee/frontend/src/views/HomeView.vue
+++ p4th/YeongyeongLee/frontend/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+
+
+<P>메인페이지</P>
+
+
+
+
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+// @ is an alias to /src
 
-@Component({
+
+export default {
+  name: 'HomeView',
   components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+
+  }
+}
 </script>
