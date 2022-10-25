b7b124a52de802bda28bfa561e624ff63332b5a5
joanna930224 <annavu0224@gmail.com>
2022-10-25 20:55:49 +0900

    init: [조안나] 개인 저장소 초기화

---
+++ p4th/AnnaJo/lolEsportsKorea_frontend/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
