b27f15258ea0ebdf3d4c2bc8352159d0784689ae
Hanchaeseon <mdmodina34@gmail.com>
2022-10-26 21:48:36 +0900

    feat. 개인 회원 정보 entity 생성 [READUS-8]

---
+++ p4th/ChaeseonHan/readus_backend/src/main/java/kr/eddi/demo/entity/Authentication.java
@@ -0,0 +1,39 @@
+package kr.eddi.demo.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@ToString(exclude = "member")
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "authentication_type")
+public abstract class Authentication {
+
+    public static final String BASIC_AUTH = "BASIC";
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id = null;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private ReadUsMember member;
+
+    @Column(name = "authentication_type", nullable = false, insertable = false, updatable = false)
+    private String authenticationType;
+
+    public Authentication(ReadUsMember member, String authenticationType) {
+        this.member = member;
+        this.authenticationType = authenticationType;
+    }
+
+
+
+
+}
