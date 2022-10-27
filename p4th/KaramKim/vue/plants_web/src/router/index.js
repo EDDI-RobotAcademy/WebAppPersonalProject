51b11bd865ab91dc9e0a3e0690a8b3631603fb98
Karam Kim <kim.ram454@gmail.com>
2022-10-27 17:22:55 +0900

    init: [김가람] 개인 저장소 초기화 - frontend

--- p4th/KaramKim/vue/plants_web/src/router/index.js
+++ p4th/KaramKim/vue/plants_web/src/router/index.js
@@ -10,14 +10,6 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
--- p4th/KaramKim/vue/plants_web/src/views/HomeView.vue
+++ p4th/KaramKim/vue/plants_web/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <hello-world/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+
+  import HelloWorld from "@/components/HelloWorld";
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HelloWorld
+    },
   }
-}
 </script>
