788f3c2b5259ee4effc789dfc68e2c81d61135dc
kookiki91 <kookiki91@naver.com>
2022-10-27 18:03:39 +0900

    init : [이창훈] 개인 저장소 cookie 추가 및 mutation-types 누락으로 추가

---
+++ p4th/ChanghoonLee/trade_frontend/vue/frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        'vue/multi-word-component-names': ['error', {
+            'ignores': ['Home']
+        }]
+    }
+};
