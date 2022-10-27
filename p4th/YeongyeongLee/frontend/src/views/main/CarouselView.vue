134c6bd673da72b1c7e9e44b5ea50c3f0fb84f77
k724k <k724k@naver.com>
2022-10-27 15:56:50 +0900

    feat: [이연경] Carousel UI [TRIP-COMMUNITY1]

---
+++ p4th/YeongyeongLee/frontend/src/views/main/CarouselView.vue
@@ -0,0 +1,59 @@
+
+<template>
+  <div class="slide-3d">
+    <v-carousel :cycle="cycle" :interval="interval" :height="height" v-model="model">
+      <v-carousel-item
+          v-for="(picture, i) in pictures"
+          :key="i">
+
+        <v-sheet
+            :height="height"
+            tile>
+
+          <v-row
+              class="fill-height"
+              align="center"
+              justify="center">
+
+            <div class="text-h2">
+
+            <v-img :src="require('@/assets/img/1.jpg')" :height="height" contain></v-img>
+            <v-img :src="require('@/assets/img/2.jpg')" :height="height" contain></v-img>
+
+
+
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "CarouselTestView",
+  data: () => ({
+    model: 0,
+    pictures:[
+      {img: require("@/assets/img/1.jpg"),},
+
+      {img: require("@/assets/img/2.jpg"),},
+
+      {img: require("@/assets/img/3.jpg"),},
+
+
+    ],
+
+    cycle: true,
+    interval: 3000,
+    height: 500,
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
