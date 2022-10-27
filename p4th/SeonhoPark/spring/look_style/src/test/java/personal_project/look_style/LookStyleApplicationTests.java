bb141a2d7a1dbff4663c7de70e17952219be3535
parkseonho1 <preferr1@naver.com>
2022-10-27 16:43:08 +0900

    init: [박선호] 개인 저장소 초기화

---
+++ p4th/SeonhoPark/spring/look_style/src/test/java/personal_project/look_style/LookStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package personal_project.look_style;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
