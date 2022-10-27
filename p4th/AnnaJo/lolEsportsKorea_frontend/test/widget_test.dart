675934f3e396201beb021ba82e620e307c7bfbbf
joanna930224 <annavu0224@gmail.com>
2022-10-28 00:57:28 +0900

    init: [조안나] 개인 저장소 초기화

--- p4th/AnnaJo/lolEsportsKorea_frontend/test/widget_test.dart
+++ p4th/AnnaJo/lolEsportsKorea_frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_login_ui/main.dart';
+import 'package:lol_esports_korea_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
