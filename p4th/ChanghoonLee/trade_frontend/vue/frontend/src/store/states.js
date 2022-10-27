b00a14b3ac9cfc7332910726594aa9eb03c717d4
kookiki91 <kookiki91@naver.com>
2022-10-27 18:03:39 +0900

    init : [이창훈] 개인 저장소 초기화

---
+++ p4th/ChanghoonLee/trade_frontend/vue/frontend/src/store/states.js
@@ -0,0 +1,13 @@
+export default {
+    buyBoards: [],
+    buyBoard: {},
+
+    freeBoards: [],
+    freeBoard: {},
+
+    reportBoards: [],
+    reportBoard: {},
+
+    sellBoards: [],
+    sellBoard: {},
+}
