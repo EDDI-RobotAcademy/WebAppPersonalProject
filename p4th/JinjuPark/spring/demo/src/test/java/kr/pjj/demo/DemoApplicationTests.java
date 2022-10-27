5497b35f9432172a6ec1c465efc3392a2322987b
dobbymun <tearofcake@gmail.com>
2022-10-27 16:35:07 +0900

    init: [박진주] 개인 저장소 초기화

---
+++ p4th/JinjuPark/spring/demo/src/test/java/kr/pjj/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pjj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
