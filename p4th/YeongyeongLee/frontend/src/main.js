3b9cc8f910f1693f59a64320254aadfa1970644c
k724k <k724k@naver.com>
2022-10-28 17:42:30 +0900

    init: [이연경]  Carousel UI 추가 코드 커밋 [TRIP-COMMUNITY1]

---
+++ p4th/YeongyeongLee/frontend/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import CommonButton from '@/components/common/CommonButton'
+
+
+Vue.component('CommonButton', CommonButton)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
