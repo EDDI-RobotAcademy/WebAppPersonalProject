cc2f4e6053feacce3d184d5de3a1554d296ec86d
soyeonkim96 <shimmer326@gmail.com>
2022-10-28 19:23:29 +0900

    init: [김소연] 회원가입 UI

--- p4th/SoyeonKim/frontend/looklend/src/views/HomeView.vue
+++ p4th/SoyeonKim/frontend/looklend/src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
-  <hello-world />
+  <v-container>
+    <div id="home">
+      <nav>
+        <router-link to="/vuetify-taste">Vuetify 예시들</router-link> |
+        <router-link to="/sign-up">회원 가입</router-link> |
+      </nav>
+      <router-view/>
+    </div>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+// @ is an alias to /src
 
   export default {
     name: 'HomeView',
 
     components: {
-      HelloWorld,
     },
   }
 </script>
