b955c18a33d6f849242dbfd3cfad4eaee052d6d7
k724k <k724k@naver.com>
2022-10-28 17:43:08 +0900

    init: [이연경]  Carousel UI 추가 코드 커밋 [TRIP-COMMUNITY1]

--- p4th/YeongyeongLee/frontend/src/views/HomeView.vue
+++ p4th/YeongyeongLee/frontend/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <template>
+  <div>
+    <MemberButtonView/>
+    <br><br><br>
+    <CarouselTestView/>
+    <ToolbarView/>
 
 
-<P>메인페이지</P>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
+    <FooterView/>
 
+  </div>
 
 
 </template>
@@ -12,10 +20,28 @@
 // @ is an alias to /src
 
 
+import CarouselTestView from "@/views/main/CarouselView";
+import ToolbarView from "@/views/main/ToolbarView";
+import FooterView from "@/views/main/FooterView";
+import MemberButtonView from "@/views/main/member/MemberButtonView";
+
+
 export default {
   name: 'HomeView',
   components: {
+    MemberButtonView,
+    CarouselTestView,
+    ToolbarView,
+    FooterView,
 
-  }
+
+  },
+
+
 }
 </script>
+
+<style>
+
+
+</style>
