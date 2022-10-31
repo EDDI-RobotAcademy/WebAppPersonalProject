f28126743d24066e314b945f87dc30265538e61e
choijaemin <foertonw5@naver.com>
2022-10-31 17:22:33 +0900

    feat : [최재민]회원 로그인 테스트 케이스 통과 [Leaning-helper3]

--- p4th/JaeminChoi/spring/LeaningHelper/src/test/java/kr/eddi/LeaningHelper/MemberServiceTestCase.java
+++ p4th/JaeminChoi/spring/LeaningHelper/src/test/java/kr/eddi/LeaningHelper/MemberServiceTestCase.java
@@ -1,5 +1,6 @@
 package kr.eddi.LeaningHelper;
 
+import kr.eddi.LeaningHelper.request.member.MemberSignInRequest;
 import kr.eddi.LeaningHelper.service.member.MemberServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,16 @@ public class MemberServiceTestCase {
     void checkId(){
         String ID = "Test1@test1.com";
         boolean outcome = service.emailValidation(ID);
+
+        System.out.println("결과 : " + outcome );
+    }
+
+    @Test
+    void checkSignIn(){
+        MemberSignInRequest memberSignInTestRequest = new MemberSignInRequest("Test1@test1.com" , "123");
+        String ID = "Test1@test1.com";
+
+        String outcome = service.signIn(memberSignInTestRequest);
 
         System.out.println("결과 : " + outcome );
     }
