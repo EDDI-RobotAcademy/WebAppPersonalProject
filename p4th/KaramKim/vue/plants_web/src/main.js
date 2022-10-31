7c29cbbe50a329168063714a672f853529a56aec
Karam Kim <kim.ram454@gmail.com>
2022-10-31 16:48:08 +0900

    feat: [김가람] F) 모든 화면에서 사용되는 공통 메뉴 UI - 추가 코드 [PLANTS2]

--- p4th/KaramKim/vue/plants_web/src/main.js
+++ p4th/KaramKim/vue/plants_web/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import CommonMenuBar from "@/components/common/CommonMenuBar";
+
+Vue.component('CommonMenuBar', CommonMenuBar)
 
 Vue.config.productionTip = false
 
