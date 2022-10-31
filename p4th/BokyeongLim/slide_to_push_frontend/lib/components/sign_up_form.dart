5dd4ebb6f6fd68c202d1a339620926af5c18c9ef
tonowoods <tono.woods@gmail.com>
2022-10-31 13:21:57 +0900

    feat: 서버에서 처리한 이메일 중복 체크 값을 받아 프론트로 결과값을 보여주는 UI 기능 추가 [SLIDE-TO-PUSH10]

--- p4th/BokyeongLim/slide_to_push_frontend/lib/components/sign_up_form.dart
+++ p4th/BokyeongLim/slide_to_push_frontend/lib/components/sign_up_form.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:slide_to_push_frontend/api/spring_api.dart';
-import 'package:slide_to_push_frontend/components/inputs/text_field_common.dart';
+import 'package:slide_to_push_frontend/components/inputs/text_field_nickName.dart';
 import 'package:slide_to_push_frontend/components/inputs/text_field_email.dart';
 import 'package:slide_to_push_frontend/components/inputs/text_field_password.dart';
 import 'package:slide_to_push_frontend/components/inputs/text_field_checked_password.dart';
@@ -10,6 +8,7 @@ import 'package:slide_to_push_frontend/utility/buttons.dart';
 import 'package:slide_to_push_frontend/utility/color.dart';
 import 'package:slide_to_push_frontend/utility/size.dart';
 
+import '../api/http_service_api.dart';
 import 'inputs/checkBox_field.dart';
 
 class SignUpForm extends StatefulWidget {
@@ -21,8 +20,40 @@ class SignUpForm extends StatefulWidget {
 
 class _SignUpFormState extends State<SignUpForm> {
 
-  String firstPassword = TextFieldPassword.password;
-  String secondPassword = TextFieldCheckedPassword.checkedPassword;
+  late TextEditingController emailEditController;
+  bool isButtonActive = true;
+
+  @override
+  void initState() {
+    super.initState();
+    emailEditController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    emailEditController.dispose();
+    super.dispose();
+  }
+
+  checkAccountEmail(String email) async{
+    await HttpService().emailValidation(email);
+    if(HttpService.resEmailValidation.statusCode == 200) {
+      var responseBody = jsonDecode(HttpService.resEmailValidation.body);
+      debugPrint(responseBody.toString());
+
+      if(responseBody == true) {
+        debugPrint("프론트로 넘어옴");
+        setState(() {
+          isButtonActive = false;
+        });
+      } else {
+        alertNotAvailable();
+        setState(() {
+          isButtonActive = true;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,12 +64,26 @@ class _SignUpFormState extends State<SignUpForm> {
           children: [
             TextFieldCommon(formName: '닉네임'),
             SizedBox(height: medium_gap),
-            TextFieldEmail(),
+            TextFieldEmail(controller: emailEditController,),
             SizedBox(height: small_gap),
             ElevatedButton(
-              onPressed: (){} ,
-              child: Text("이메일 중복 체크",style: TextStyle(color: ColorPicker.defaultBlack)),
-              style: Buttons.subButton2,
+              onPressed: isButtonActive
+                ? (){
+                if(emailEditController.text.isNotEmpty) {
+                  checkAccountEmail(emailEditController.text);
+                }
+
+                if(emailEditController.text.isEmpty) {
+                  alertEnterEmail();
+                  setState(() {
+                    isButtonActive = true;
+                  });
+                }
+
+              }
+              : null,
+              child: Text("이메일 중복 체크"),
+              style: Buttons.subLongButton,
             ),
             SizedBox(height: medium_gap),
             TextFieldPassword(),
@@ -62,7 +107,7 @@ class _SignUpFormState extends State<SignUpForm> {
                 } else {
                   SpringApi().signUp(SignUpRequest(
                       TextFieldCommon.enteredMessage,
-                      TextFieldEmail.email,
+                      emailEditController.text,
                       TextFieldPassword.password
                   ));
                 }
@@ -80,10 +125,27 @@ class _SignUpFormState extends State<SignUpForm> {
         )
     );
   }
+
 }
 
 int compareToString(String msg1, String msg2){
   return msg1.compareTo(msg2);
+}
+
+void alertNotAvailable() {
+  Fluttertoast.showToast(
+    msg:"중복된 이메일입니다.",
+    gravity: ToastGravity.TOP,
+    toastLength: Toast.LENGTH_SHORT,
+  );
+}
+
+void alertEnterEmail() {
+  Fluttertoast.showToast(
+    msg:"이메일을 입력해주세요.",
+    gravity: ToastGravity.TOP,
+    toastLength: Toast.LENGTH_SHORT,
+  );
 }
 
 void failDueToUnchecked() {
