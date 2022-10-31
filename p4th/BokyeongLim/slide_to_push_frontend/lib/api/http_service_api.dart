0cb0097e6eaefa241767ff2a2cc845016b4379af
tonowoods <tono.woods@gmail.com>
2022-10-31 17:59:45 +0900

    feat: 가입정보를 서버와 주고받을 API 리팩토링 [SLIDE-TO-PUSH11]

--- p4th/BokyeongLim/slide_to_push_frontend/lib/api/http_service_api.dart
+++ p4th/BokyeongLim/slide_to_push_frontend/lib/api/http_service_api.dart
@@ -2,10 +2,14 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:slide_to_push_frontend/api/states/account_info.dart';
 
 class HttpService {
   static const String httpUri = '10.0.2.2:9955';
   static var resEmailValidation;
+  static var resSignUp;
+  static var resSignIn;
+
   emailValidation(String email) async {
     var data = {'email': email};
     var body = json.encode(data);
@@ -18,6 +22,20 @@ class HttpService {
       );
 
     }catch (e){
+      debugPrint(e.toString());
+    }
+  }
+
+  signUpService(Account account) async {
+    var body = json.encode(account);
+
+    try {
+      resSignUp = await http.post(
+        Uri.http(httpUri, '/member/sign-up'),
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+    } catch (e) {
       debugPrint(e.toString());
     }
   }
