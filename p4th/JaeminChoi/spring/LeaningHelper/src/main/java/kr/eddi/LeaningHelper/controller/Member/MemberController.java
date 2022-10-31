dfd418174e8d3e432ca386dd53a43aa00a350509
choijaemin <foertonw5@naver.com>
2022-10-31 17:21:00 +0900

    feat : [최재민] API를 통해 받은 정보로 회원 로그인 엔티티 구현[Leaning-helper3]

--- p4th/JaeminChoi/spring/LeaningHelper/src/main/java/kr/eddi/LeaningHelper/controller/Member/MemberController.java
+++ p4th/JaeminChoi/spring/LeaningHelper/src/main/java/kr/eddi/LeaningHelper/controller/Member/MemberController.java
@@ -1,4 +1,24 @@
 package kr.eddi.LeaningHelper.controller.Member;
 
+import kr.eddi.LeaningHelper.form.MemberSignInForm;
+import kr.eddi.LeaningHelper.service.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/member")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MemberController {
+
+    @Autowired
+    private MemberService service;
+    @PostMapping("/sing-in")
+    public String memberSinIn(@RequestBody MemberSignInForm memberSignInForm){
+        log.info("memberSinIn(): " + memberSignInForm);
+
+        return service.signIn(memberSignInForm.toMemberSignInRequest());
+    }
+
 }
