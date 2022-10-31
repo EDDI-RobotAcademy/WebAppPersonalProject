0845d7b29efac57958221166bf6d06ae3a631fda
choijaemin <foertonw5@naver.com>
2022-10-31 17:22:00 +0900

    feat : [최재민] API를 통해 받은 정보로 회원 로그인 서비스 구현[Leaning-helper3]

--- p4th/JaeminChoi/spring/LeaningHelper/src/main/java/kr/eddi/LeaningHelper/service/member/MemberService.java
+++ p4th/JaeminChoi/spring/LeaningHelper/src/main/java/kr/eddi/LeaningHelper/service/member/MemberService.java
@@ -1,13 +1,14 @@
 package kr.eddi.LeaningHelper.service.member;
 
 import kr.eddi.LeaningHelper.request.member.MemberRegisterRequest;
+import kr.eddi.LeaningHelper.request.member.MemberSignInRequest;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface MemberService {
     public Boolean memberRegister(MemberRegisterRequest memberRegisterRequest);
 
     public Boolean emailValidation(String id);
 
+    public String signIn(MemberSignInRequest memberSignInRequest);
 
 }
