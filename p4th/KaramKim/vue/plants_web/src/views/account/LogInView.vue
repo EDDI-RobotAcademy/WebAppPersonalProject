a3390598a6df18a9d64d0a220291bd203b56da57
Karam Kim <kim.ram454@gmail.com>
2022-10-31 21:07:07 +0900

    feat: [김가람] f) 모든 사용자가 로그인이 필요한 화면에서 보는 로그인 UI - 구현중 [PLANTS4]

---
+++ p4th/KaramKim/vue/plants_web/src/views/account/LogInView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <common-menu-bar/>
+    <log-in-form @submit="onSubmit"/>
+  </div>
+</template>
+
+<script>
+import LogInForm from "@/components/account/LogInForm";
+export default {
+  name: "LogInView",
+  components: {
+    LogInForm
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
