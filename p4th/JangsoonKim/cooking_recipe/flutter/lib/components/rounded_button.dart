215faa5411fc10cf56bef2aded8ee2d837c3ad60
jssnk <ssnssnl@kakao.com>
2022-11-02 00:02:39 +0900

    feat: [김장순] Cooking-Recipe 어플 내에서 재사용 가능한 공통 컴포넌트[Cooking-Recipe3]

---
+++ p4th/JangsoonKim/cooking_recipe/flutter/lib/components/rounded_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback press;
+  final Color color, textColor;
+  const RoundedButton({Key? key,
+    required this.text,
+    required this.press,
+    required this.color,
+    required this.textColor
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: TextButton(
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+              backgroundColor: color,
+            ),
+            onPressed: press,
+            child: Text(text,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: textColor))),
+      ),
+    );
+  }
+}
