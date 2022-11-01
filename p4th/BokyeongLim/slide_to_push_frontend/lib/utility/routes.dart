eff6c49c5154a68fce0a892134ee35e07a88cf2b
tonowoods <tono.woods@gmail.com>
2022-11-01 23:06:04 +0900

    feat: 전달받은 토큰 저장 및 새로운 페이지로 이동 시 토큰 전달을 확인할 테스트 페이지 작성 [SLIDE-TO-PUSH12]

--- p4th/BokyeongLim/slide_to_push_frontend/lib/utility/routes.dart
+++ p4th/BokyeongLim/slide_to_push_frontend/lib/utility/routes.dart
@@ -2,15 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:slide_to_push_frontend/view/sign_in_view.dart';
 import 'package:slide_to_push_frontend/view/sign_up_view.dart';
 
+import '../components/test_view.dart';
+
 class Routes {
   Routes._();
 
   static const String signIn = '/sign_in';
   static const String signUp = '/sign_up';
+  static const String testView = '/test_home_view';
 
   static final routes = <String, WidgetBuilder>{
     signIn: (BuildContext context) => SignInView(),
     signUp: (BuildContext context) => SignUpView(),
-
+    testView: (BuildContext context) => TestHomeView(account: 'account',),
   };
 }
