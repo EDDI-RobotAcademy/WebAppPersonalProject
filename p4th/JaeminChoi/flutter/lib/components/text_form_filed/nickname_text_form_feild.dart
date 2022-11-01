dd3d95442a749add51f0e5cebc78d26f2b531c29
choijaemin <foertonw5@naver.com>
2022-11-01 17:52:17 +0900

    feat: [최재민] NickName_text_form 작성 및 NicNaneValidate 추가[Leaning-Helper]

---
+++ p4th/JaeminChoi/flutter/lib/components/text_form_filed/nickname_text_form_feild.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../../utility/decorations/text_form_decoration.dart';
+import '../../utility/form_validate.dart';
+import '../../utility/size.dart';
+
+
+class NickNameTextFormField extends StatefulWidget {
+  const NickNameTextFormField({Key? key , required this.widthSize}) : super(key: key);
+
+  static String nickName = '';
+  final double widthSize;
+  @override
+  State<NickNameTextFormField> createState() => _NickNameTextFormFieldState();
+}
+
+class _NickNameTextFormFieldState extends State<NickNameTextFormField> {
+
+  FocusNode _NickNameFocus = new FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.widthSize,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: medium_gap,),
+          TextFormField(
+            decoration: textFormDecoration("닉네임"),
+            keyboardType: TextInputType.text,
+            focusNode: _NickNameFocus,
+            autovalidateMode : AutovalidateMode.onUserInteraction ,
+            validator: (value) => CheckValidate().validateNickName(_NickNameFocus, value!),
+            onSaved: (value) {
+              setState(()
+              {NickNameTextFormField.nickName = value!;}
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
