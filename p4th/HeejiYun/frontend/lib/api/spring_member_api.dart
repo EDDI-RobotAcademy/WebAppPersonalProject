a4ae52ef0e5a7f64e03566c067ef8e32c051f1ec
HEEJI YUN <yhj8147e@gmail.com>
2022-11-04 16:35:41 +0900

    feat: [윤희지] 회원가입 요청을 위한 api 구현 완료 [HOME-ALONE9]

--- p4th/HeejiYun/frontend/lib/api/spring_member_api.dart
+++ p4th/HeejiYun/frontend/lib/api/spring_member_api.dart
@@ -5,9 +5,8 @@ import 'package:http/http.dart' as http;
 
 class SpringMemberApi {
   static const String httpUri = '192.168.1.102:7779';
-  static dynamic emailPass = '';
 
-  Future<dynamic> emailCheck ( String email ) async {
+  Future<bool?> emailCheck ( String email ) async {
     var data = { 'email': email };
     var body = json.encode(data);
 
@@ -21,25 +20,28 @@ class SpringMemberApi {
       debugPrint("통신 확인");
     }
 
-    emailPass = response.body;
-    debugPrint(emailPass);
+    return json.decode(response.body);
+
   }
 
-  Future<MemberSignUpResponse> nicknameCheck (String nickname) async {
+  Future<bool?> nicknameCheck (String nickname) async {
+    var data = { 'nickname': nickname };
+    var body = json.encode(data);
 
     var response = await http.post(
       Uri.http(httpUri, '/member/check-nickname/$nickname'),
       headers: {"Content-Type": "application/json"},
+      body: body,
     );
 
     if (response.statusCode == 200) {
       debugPrint("통신 확인");
     }
 
-    return MemberSignUpResponse(true);
+    return json.decode(response.body);
   }
 
-  Future<MemberSignUpResponse> signUp (MemberSignUpRequest request) async {
+  Future<bool?> signUp (MemberSignUpRequest request) async {
     var data = { 'email': request.email, 'password': request.password, 'nickname': request.nickname };
     var body = json.encode(data);
 
@@ -58,7 +60,7 @@ class SpringMemberApi {
       debugPrint("통신 확인");
     }
 
-    return MemberSignUpResponse(true);
+    return json.decode(response.body);
   }
 
   Future<UserLoginResponse> login (UserLoginRequest request) async {
@@ -71,7 +73,6 @@ class SpringMemberApi {
 
     var response = await http.post(
       Uri.http(httpUri, '/member/sign-up'),
-      //Uri.http(httpUri, '/58th/member/sign-in'),
       headers: {"Content-Type": "application/json"},
       body: body,
     );
@@ -84,30 +85,12 @@ class SpringMemberApi {
   }
 }
 
-class EmailCheckResponse {
-  bool success;
-
-  EmailCheckResponse({required this.success});
-
-  factory EmailCheckResponse.fromJson(bool json) {
-    return EmailCheckResponse(
-      success: json
-    );
-  }
-}
-
 class MemberSignUpRequest {
   String email;
   String password;
   String nickname;
 
   MemberSignUpRequest(this.email, this.password, this.nickname);
-}
-
-class MemberSignUpResponse {
-  bool? success;
-
-  MemberSignUpResponse(this.success);
 }
 
 class UserLoginResponse {
