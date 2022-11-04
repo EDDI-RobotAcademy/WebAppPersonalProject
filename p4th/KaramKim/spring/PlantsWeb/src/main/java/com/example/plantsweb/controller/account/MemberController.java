f0e44a31d112c20253a7bc701073f37331abd850
Karam Kim <kim.ram454@gmail.com>
2022-11-04 22:50:35 +0900

    feat: [김가람] B) 회원가입과 로그인에 필요한 회원 엔티티 및 컨트롤러 설계 [PLANTS8]

---
+++ p4th/KaramKim/spring/PlantsWeb/src/main/java/com/example/plantsweb/controller/account/MemberController.java
@@ -0,0 +1,13 @@
+package com.example.plantsweb.controller.account;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/plants/member")
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
+public class MemberController {
+
+
+}
