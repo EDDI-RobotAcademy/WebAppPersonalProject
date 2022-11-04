23d8e626af010146ea500e237e57ccc5051f50f6
Karam Kim <kim.ram454@gmail.com>
2022-11-05 00:44:49 +0900

    feat: [김가람] F) 회원가입 정보에 이름 추가 및 회원가입 요청 시 서버 통신 기능 구현 [PLANTS3]

--- p4th/KaramKim/vue/plants_web/src/views/account/SignUpView.vue
+++ p4th/KaramKim/vue/plants_web/src/views/account/SignUpView.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <sign-up-form/>
-  </div>
+  <v-container>
+    <sign-up-form @submit="onSubmit"/>
+  </v-container>
 </template>
 
 <script>
 import SignUpForm from "@/components/account/SignUpForm";
+import {mapActions} from "vuex";
 export default {
   name: "SignUpView",
-  components: {SignUpForm}
+  components: {SignUpForm},
+  methods: {
+    ...mapActions([
+       'requestSignUpDataToSpring'
+    ]),
+    onSubmit (payload) {
+      this.requestSignUpDataToSpring(payload)
+    }
+  }
 }
 </script>
 
