a1d550e82b5bc2181ffe10e8f19ba088204860f3
tonowoods <tono.woods@gmail.com>
2022-11-06 21:57:08 +0900

    feat: 사용자가 투두를 체크할 스위치 UI 추가 및 쓰기 버튼 추가[SLIDE-TO-PUSH16]

--- p4th/BokyeongLim/slide_to_push_frontend/lib/view/my_todo_view.dart
+++ p4th/BokyeongLim/slide_to_push_frontend/lib/view/my_todo_view.dart
@@ -10,6 +10,20 @@ class MyTodoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton:Container(
+        margin: EdgeInsets.fromLTRB(0, 0, 15, 10),
+        height: 75.0,
+        width: 75.0,
+        child: FittedBox(
+          child: FloatingActionButton(
+            child: Icon(
+              Icons.edit,
+            ),
+            backgroundColor: ColorPicker.defaultBlack,
+            onPressed: (){},
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text('방문자님 환영합니다.', style: TextStyle(color: Colors.black),),
         backgroundColor: Colors.transparent,
@@ -30,30 +44,32 @@ class MyTodoView extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.symmetric(
-                vertical: BorderSide(color: ColorPicker.defaultBlack, width: 5)
+        child:SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.symmetric(
+                  vertical: BorderSide(color: ColorPicker.defaultBlack, width: 5)
+              ),
+              // color: Colors.red,
             ),
-            // color: Colors.red,
-          ),
-          child: Column(
-            children: [
-              SizedBox(height: medium_gap,),
-              Text("내 응원개 수"),
-              SizedBox(height: medium_gap,),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.all(15),
-                child:Text("TODAY", style: TextStyle(color: Colors.white),), color: ColorPicker.defaultBlack,),
-              TodosList(),
-              SizedBox(height: 2.5, child: Container(color: ColorPicker.defaultBlack),),
-              TextButton(
-                onPressed: (){  },
-                child: Text('클릭해서 통신확인', style:TextStyle(color: ColorPicker.subTextButton, fontSize: 18)),
-              )
-            ],
-          ),
+            child: Column(
+              children: [
+                SizedBox(height: medium_gap,),
+                Text("내 응원개 수"),
+                SizedBox(height: medium_gap,),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.all(15),
+                  child:Text("TODAY", style: TextStyle(color: Colors.white),), color: ColorPicker.defaultBlack,),
+                TodosList(),
+                SizedBox(height: 2.5, child: Container(color: ColorPicker.defaultBlack),),
+                TextButton(
+                  onPressed: (){  },
+                  child: Text('클릭해서 통신확인', style:TextStyle(color: ColorPicker.subTextButton, fontSize: 18)),
+                ),
+              ],
+            ),
+          )
         )
       )
     );
