46dbf9d848989a50dd81d908c0f280e38c6d2922
kookiki91 <kookiki91@naver.com>
2022-11-08 03:20:04 +0900

    init : [이창훈] 회원 정보 entity 생성 [TRADE-ITEM-2]

---
+++ p4th/ChanghoonLee/trade_backend/demo/src/main/java/com/example/demo/utility/password/PasswordHashConverter.java
@@ -0,0 +1,19 @@
+package com.example.demo.utility.password;
+
+import com.example.demo.utility.encrypt.EncryptionUtil;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class PasswordHashConverter implements AttributeConverter<String, String> {
+    @Override
+    public String convertToDatabaseColumn(String attribute) {
+        return EncryptionUtil.generateHash(attribute);
+    }
+
+    @Override
+    public String convertToEntityAttribute(String dbData) {
+        return dbData;
+    }
+}
