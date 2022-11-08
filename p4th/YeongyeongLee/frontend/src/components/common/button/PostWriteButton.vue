ba4b794b6e56e2ea1864ffd6e8c9745d5c1a6d71
k724k <k724k@naver.com>
2022-11-08 21:57:21 +0900

    feat: [이연경] 나라카테고리 클릭시 UI [TRIP-COMMUNITY6]

---
+++ p4th/YeongyeongLee/frontend/src/components/common/button/PostWriteButton.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div style="float: right">
+    <v-btn :href= "'/post-write-form'">
+      <span>게시글 작성</span>
+      <v-icon>mdi-view-list-outline</v-icon>
+
+    </v-btn>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: "PostWriteButton",
+  components:{
+
+
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ p4th/YeongyeongLee/frontend/src/views/country/AsiaBoardView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <MemberButtonView/>
+    <br>
+    <p></p><br><br><br>
+    <ToolbarButton/>
+    <br>
+
+
+    <br><br>
+
+    <PostWriteButton/>
+
+
+
+  </div>
+
+
+</template>
+
+<script>
+import ToolbarButton from "@/components/common/button/ToolbarButton";
+import MemberButtonView from "@/views/mainpage/MemberButtonView";
+import PostWriteButton from "@/components/common/button/PostWriteButton";
+
+export default {
+  name: "AsiaBoardList",
+  components: {
+    MemberButtonView,
+    ToolbarButton,
+    PostWriteButton,
+
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
