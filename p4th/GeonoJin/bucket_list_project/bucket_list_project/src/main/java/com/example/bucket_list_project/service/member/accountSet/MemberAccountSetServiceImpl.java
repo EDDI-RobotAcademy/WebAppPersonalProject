468c43234a5fc3dcdf043a2f78ee0f8f8b01720f
G <shjwin96@gmail.com>
2022-11-08 18:26:32 +0900

    feat: [진건오] 로그인한 사용자가 현재 닉네임을 변경하기 위한 처리 시스템[BUCKET-LIST14]

---
+++ p4th/GeonoJin/bucket_list_project/bucket_list_project/src/main/java/com/example/bucket_list_project/service/member/accountSet/MemberAccountSetServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.bucket_list_project.service.member.accountSet;
+
+import com.example.bucket_list_project.entity.member.MemberInfo;
+import com.example.bucket_list_project.repository.member.MemberRepository;
+import com.example.bucket_list_project.service.member.request.accountSet.ReplaceNicknameRequest;
+import com.example.bucket_list_project.service.member.security.RedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class MemberAccountSetServiceImpl implements MemberAccountSetService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private RedisService redisService;
+
+    @Override
+    public void replaceNickname(ReplaceNicknameRequest nicknameRequest) {
+        String tokenValue = nicknameRequest.getCurrentUserToken();
+        Long userId = redisService.getValueByKey(tokenValue);
+
+        Optional<MemberInfo> findMember = memberRepository.findById(userId);
+
+        findMember.ifPresent(selectUser->{
+            MemberInfo userInfo = findMember.get();
+            selectUser.setEmail(userInfo.getEmail());
+            selectUser.setNickName(nicknameRequest.getNickName());
+
+            memberRepository.save(selectUser);
+        });
+    }
+}
