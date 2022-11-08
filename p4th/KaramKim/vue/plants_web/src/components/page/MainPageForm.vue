4920e7f9a3e2803df81d0bdac759b95841af8c6f
Karam Kim <kim.ram454@gmail.com>
2022-11-08 23:04:35 +0900

    Feat: [김가람] F) 모든 사용자가 처음 사이트 접속 시 보는 초기 메인 화면 UI - 구현중  [PLANTS1]

---
+++ p4th/KaramKim/vue/plants_web/src/components/page/MainPageForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="slide-3d">
+      <v-row justify="center">
+        <v-col cols="8">
+          <v-carousel
+              :cycle="cycle"
+              :interval="interval"
+              v-model="model"
+              height="500"
+              hide-delimiters
+              :show-arrows="false">
+            <v-carousel-item v-for="image in images" :key="image">
+              <v-img :src="image" contain max-height="500"></v-img>
+            </v-carousel-item>
+          </v-carousel>
+        </v-col>
+      </v-row>
+    </div>
+    <div class="subsciption" align="center">
+      <p>함께하는 식물을 등록해주세요.</p>
+    </div>
+    <div align="center" class="btn mt-10 mb-10">
+      <v-btn @click="registerPlants" rounded x-large class="light-green--text text--darken-3" color="white" width="300px">
+        <span>식물 등록하기</span>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPageForm",
+  data: () => ({
+    model: 0,
+    images: [
+      require('@/assets/img/mainPage/main1.jpg'),
+      require('@/assets/img/mainPage/main2.jpg'),
+      require('@/assets/img/mainPage/main3.jpg'),
+      require('@/assets/img/mainPage/main4.jpg'),
+    ],
+    cycle: true,
+    interval: 5000,
+    height: 500,
+  }),
+  methods: {
+    registerPlants () {
+      this.$router.push('/register-plant')
+    }
+  },
+}
+</script>
+
+<style scoped>
+.btn {
+  padding-bottom: 100px;
+}
+.subsciption {
+  padding-top: 100px;
+}
+
+
+</style>
--- p4th/KaramKim/vue/plants_web/src/views/HomeView.vue
+++ p4th/KaramKim/vue/plants_web/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-
+    <main-page-form/>
   </div>
 </template>
 
 <script>
 
 
+  import MainPageForm from "@/components/page/MainPageForm";
   export default {
     name: 'HomeView',
 
     components: {
-
-
+      MainPageForm
     },
     data () {
       return {
