ac340a1e067a8d1f2602e8444dd3c9b0cee3a81b
tonowoods <tono.woods@gmail.com>
2022-11-08 16:03:39 +0900

    feat: 달력 위젯을 사용하기 위한 패키지 설치 [SLIDE-TO-PUSH18]

--- p4th/BokyeongLim/slide_to_push_frontend/lib/main.dart
+++ p4th/BokyeongLim/slide_to_push_frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:slide_to_push_frontend/utility/color.dart';
 import 'package:slide_to_push_frontend/utility/routes.dart';
 
@@ -14,6 +15,15 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Slide to Push',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('ko', 'KO'),
+      ],
       routes: Routes.routes,
       theme: ThemeData(
         fontFamily: 'AppleSDGothicNeo',
