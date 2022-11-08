5707fdd660bceb7b1a6d0455b237ce503af70d08
kookiki91 <kookiki91@naver.com>
2022-11-08 20:47:50 +0900

    feat:[이창훈] 메인페이지 게시판 메뉴바에서 회원가입 및 로그인 버튼 분리 [TRADE-ITEM-7]

--- p4th/ChanghoonLee/trade_frontend/vue/frontend/src/components/toolbar/ToolbarForm.vue
+++ p4th/ChanghoonLee/trade_frontend/vue/frontend/src/components/toolbar/ToolbarForm.vue
@@ -2,11 +2,6 @@
   <v-container justify-center>
     <div>
       <v-toolbar dense dark>
-        <v-toolbar-title>
-          <router-link to="/">
-            <v-img :src="require('@/assets/img/로고.png')" :height=40 contain></v-img>
-          </router-link>
-        </v-toolbar-title>
         <v-toolbar-items>
           <v-btn text v-for="category in menuCategory" :key="category.text" :to="category.route">
             {{ category.text }}
@@ -31,8 +26,6 @@ export default {
         { text: '자유게시판', name: 'free-boards', route: '/' },
         { text: '신고게시판', name: 'report-boards', route: '/' },
         { text: '공지사항', name: 'notice-boards', route: '/' },
-        { text: '회원가입', name: 'sign-in', route: '/' },
-        { text: '로그인', name: 'login', route: '/' },
       ],
     }
   },
---
+++ p4th/ChanghoonLee/trade_frontend/vue/frontend/src/components/topMenubar/TopMenuForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title>
+        <router-link to="/">
+          <v-img :src="require('@/assets/img/로고.png')" :height=60 contain></v-img>
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <router-link to="/">
+        <v-btn class="grey white--text" rounded depressed small>
+          회원가입
+        </v-btn>
+      </router-link>&nbsp;
+      <router-link to="/">
+        <v-btn class="grey white--text" rounded depressed small>
+          로그인
+        </v-btn>
+      </router-link>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TopMenuForm"
+}
+</script>
+
+<style scoped>
+
+a {
+  text-decoration: none;
+}
+
+</style>
