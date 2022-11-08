3fe2df1d9556400c625ab44ea46b18aed9b4c658
Hanchaeseon <mdmodina34@gmail.com>
2022-11-08 17:58:58 +0900

    init. [한채선] package.json 변경

---
+++ p4th/ChaeseonHan/readus_backend/src/main/java/kr/eddi/demo/entity/CommunityBoard.java
@@ -0,0 +1,56 @@
+package kr.eddi.demo.entity;
+
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommunityBoard {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardNo = null;
+
+    @Column(length = 32, nullable = false)
+    private String category;
+
+    @Column(length = 128, nullable = false)
+    private String title;
+
+    @Column(length = 32, nullable = false)
+    private String writer;
+
+    @Lob
+    private String content;
+
+    @CreationTimestamp
+    private Date createdDate;
+
+    @UpdateTimestamp
+    private Date updatedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private ReadUsMember readUsMember;
+
+    public CommunityBoard(String category, String title, String content) {
+        this.category = category;
+        this.title = title;
+        this.content = content;
+        this.writer = readUsMember.getNickName();
+    }
+
+    public void modifyBoard(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
