431609f40cbbf5e7168135c93bccce6ab311cf41
tonowoods <tono.woods@gmail.com>
2022-11-08 17:22:25 +0900

    feat: 쓰기페이지 Form과 View 페이지 생성[SLIDE-TO-PUSH18]

---
+++ p4th/BokyeongLim/slide_to_push_frontend/lib/components/register_todo_form.dart
@@ -0,0 +1,83 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:slide_to_push_frontend/components/date_picker_controller.dart';
+import 'package:slide_to_push_frontend/components/inputs/text_field_common.dart';
+import 'package:slide_to_push_frontend/utility/size.dart';
+
+import '../utility/buttons.dart';
+import '../utility/color.dart';
+
+class RegisterTodoForm extends StatefulWidget {
+  const RegisterTodoForm({Key? key}) : super(key: key);
+  @override
+  State<RegisterTodoForm> createState() => _RegisterTodoFormState();
+}
+
+class _RegisterTodoFormState extends State<RegisterTodoForm> {
+  late TextEditingController todosController;
+
+  @override
+  void initState() {
+    super.initState();
+    todosController = TextEditingController();
+  }
+  @override
+  void dispose() {
+    todosController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    Get.put(DatePickerController());
+    return Form(
+        key: formKey,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Obx(() => Text(
+                    DateFormat('yyyy-MM-dd')
+                        .format(Get.find<DatePickerController>()
+                        .selectedDate.value)
+                        .toString(),
+                    style: TextStyle(fontSize: 20)
+                )),
+                SizedBox(width: small_gap,),
+                TextButton(
+                  onPressed: () => Get.find<DatePickerController>().chooseDate(context),
+                  child: Text('날짜 선택',
+                    style: TextStyle(
+                      fontFamily: 'AppleSDGothicNeoB',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(width: MediaQuery.of(context).size.width, height:4.0, child: Container(color: ColorPicker.defaultBlack),),
+            SizedBox(height: medium_gap,),
+            TextFieldCommon(controller: todosController, formName: '할 일'),
+            SizedBox(height: medium_gap,),
+            ElevatedButton(
+              onPressed:(){
+                formKey.currentState?.save();
+              },
+              child:Text("등록", style: TextStyle(color: Colors.white,)),
+              style: Buttons.defaultButton,
+            ),
+            SizedBox(height: medium_gap),
+            TextButton(
+              onPressed: () => Navigator.pop(context, false),
+              child: Text('Back', style:TextStyle(color: ColorPicker.subTextButton, fontSize: 18)),
+              style: Buttons.defaultTextButton,
+            )
+          ],
+        ),
+    );
+  }
+}
