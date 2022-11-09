7b4981e2b01b082efef85336833aceb8d068c904
Hanchaeseon <mdmodina34@gmail.com>
2022-11-09 19:09:05 +0900

    feat.[한채선] 모든 화면에서 유지될 footer [READUS-1]

--- p4th/ChaeseonHan/readus_frontend/src/App.vue
+++ p4th/ChaeseonHan/readus_frontend/src/App.vue
@@ -6,15 +6,17 @@
         <router-view/>
       </v-container>
     </v-main>
+    <footer-view/>
   </v-app>
 </template>
 
 <script>
 
 import HeaderView from "@/views/HeaderView";
+import FooterView from "@/views/FooterView";
 export default {
   name: 'App',
-  components: { HeaderView },
+  components: {FooterView, HeaderView },
   data: () => ({
     //
   }),
---
+++ p4th/ChaeseonHan/readus_frontend/src/views/FooterView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="background-color: #356859" class="white--text text-sm-caption">
+    <v-container>
+      © 2022 ReadUs All right reserved.
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterView"
+}
+</script>
+
+<style scoped>
+
+</style>
