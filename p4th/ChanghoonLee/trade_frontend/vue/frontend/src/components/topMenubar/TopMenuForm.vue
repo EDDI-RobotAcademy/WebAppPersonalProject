cf977291eb308dc59e40aeb977b2f037f00660c6
kookiki91 <kookiki91@naver.com>
2022-11-10 20:48:51 +0900

    feat:[이창훈] 로그인 사용자만 보이는 마이페이지 버튼과 로그아웃 버튼 UI [TRADE-ITEM-11]

--- p4th/ChanghoonLee/trade_frontend/vue/frontend/src/components/topMenubar/TopMenuForm.vue
+++ p4th/ChanghoonLee/trade_frontend/vue/frontend/src/components/topMenubar/TopMenuForm.vue
@@ -1,6 +1,25 @@
 <template>
   <nav>
-    <v-toolbar flat app>
+    <v-toolbar flat app v-if="$store.state.isAuthenticated == true">
+      <v-toolbar-title>
+        <router-link to="/">
+          <v-img :src="require('@/assets/img/로고.png')" :height=60 contain></v-img>
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <router-link to="/">
+        <v-btn class="grey white--text" rounded depressed small>
+          마이페이지
+        </v-btn>
+      </router-link>&nbsp;
+      <router-link to="/">
+        <v-btn class="grey white--text" rounded depressed small>
+          로그아웃
+        </v-btn>
+      </router-link>
+    </v-toolbar>
+    <v-toolbar flat app v-else>
       <v-toolbar-title>
         <router-link to="/">
           <v-img :src="require('@/assets/img/로고.png')" :height=60 contain></v-img>
