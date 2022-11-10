8e5543cb1108888ebc4861ea9ae2d70050fb91d0
Hanchaeseon <mdmodina34@gmail.com>
2022-11-10 19:56:51 +0900

    feat.[한채선] 탑 스크롤 버튼 구현 [READUS-24]

--- p4th/ChaeseonHan/readus_frontend/src/App.vue
+++ p4th/ChaeseonHan/readus_frontend/src/App.vue
@@ -2,9 +2,22 @@
   <v-app>
     <header-view/>
     <v-main>
-      <v-container>
+      <v-container id="scroll-target" class="scroll-y">
         <router-view/>
       </v-container>
+      <v-btn
+          v-scroll="onScroll"
+          v-show="fab"
+          fab
+          dark
+          fixed
+          bottom
+          right
+          color="#356859"
+          @click="toTop"
+      >
+        <v-icon color="white">mdi-arrow-up</v-icon>
+      </v-btn>
     </v-main>
     <footer-view/>
   </v-app>
@@ -17,8 +30,20 @@ import FooterView from "@/views/FooterView";
 export default {
   name: 'App',
   components: {FooterView, HeaderView },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      fab: false
+    }
+  },
+  methods: {
+    onScroll (e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset ||   e.target.scrollTop || 0
+      this.fab = top > 20
+    },
+    toTop () {
+      this.$vuetify.goTo(0)
+    }
+  }
 };
 </script>
