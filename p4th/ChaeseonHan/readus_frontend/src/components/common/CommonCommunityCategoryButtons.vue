53cdb5c3e37c85680409ed31d23681bf9dc8bf12
Hanchaeseon <mdmodina34@gmail.com>
2022-11-11 23:04:56 +0900

    feat.[한채선] 카테고리별 게시글 리스트 페이지 생성 및 카테고리 버튼에 라우터 추가 [READUS-26]

--- p4th/ChaeseonHan/readus_frontend/src/components/common/CommonCommunityCategoryButtons.vue
+++ p4th/ChaeseonHan/readus_frontend/src/components/common/CommonCommunityCategoryButtons.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col>
-      <v-btn class="mr-2 mb-2" color="#356859" outlined small v-for="item in category" :key="item.index">
+      <v-btn class="mr-2 mb-2" color="#356859" outlined small v-for="item in category" :key="item.index" :to="item.route">
         {{ item.title }}
       </v-btn>
     </v-col>
@@ -15,9 +15,10 @@ export default {
     return {
       writeButton: "글쓰기",
       category: [ // 네임이랑 라우터 추가하기
-        { index: 1, title: "정보" },
-        { index: 2, title: "리뷰" },
-        { index: 3, title: "일상" },
+/*        { index: 1, title: "통합", name: 'CommunityView', route: '/community'},*/
+        { index: 2, title: "정보", name: 'InformationCategory', route: '/community/information'},
+        { index: 3, title: "리뷰", name: 'ReviewCategory', route: '/community/review'},
+        { index: 4, title: "일상", name: 'LifeCategory', route: '/community/life' },
       ]
     }
   }
