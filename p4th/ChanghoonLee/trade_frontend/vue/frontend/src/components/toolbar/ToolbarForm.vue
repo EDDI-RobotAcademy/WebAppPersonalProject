8eac12e503f29b126a2bd4354fc32843a6577e1e
kookiki91 <kookiki91@naver.com>
2022-11-11 20:59:31 +0900

    feat:[이창훈] 메뉴바 메뉴 드롭다운 UI [TRADE-ITEM-15]

--- p4th/ChanghoonLee/trade_frontend/vue/frontend/src/components/toolbar/ToolbarForm.vue
+++ p4th/ChanghoonLee/trade_frontend/vue/frontend/src/components/toolbar/ToolbarForm.vue
@@ -1,8 +1,38 @@
 <template>
   <v-container justify-center>
     <div>
-      <v-toolbar dense dark>
+      <v-toolbar dense dark color="grey">
         <v-toolbar-items>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn text v-on="on">
+                아이템 구매
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item v-for="(buyItemDrop, index) in buyItemDrop" :key="index" :to="buyItemDrop.route">
+                <v-list-item-title>
+                  {{ buyItemDrop.title }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn text v-on="on">
+                아이템 판매
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item v-for="(sellItemDrop, index) in sellItemDrop" :key="index" :to="sellItemDrop.route">
+                <v-list-item-title>
+                  {{ sellItemDrop.title }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
           <v-btn text v-for="category in menuCategory" :key="category.text" :to="category.route">
             {{ category.text }}
           </v-btn>
@@ -21,12 +51,24 @@ export default {
   data() {
     return {
       menuCategory: [
-        { text: '아이템 구매', name: 'buy-item-boards', route: '/' },
-        { text: '아이템 판매', name: 'sell-item-boards', route: '/' }, // 루트 알맞게 설정
-        { text: '자유게시판', name: 'free-boards', route: '/' },
-        { text: '신고게시판', name: 'report-boards', route: '/' },
-        { text: '공지사항', name: 'notice-boards', route: '/' },
+        { text: '자유게시판', route: '/' },
+        { text: '신고게시판', route: '/' },
+        { text: '공지사항', route: '/' },
+        { text: '포인트 충전', route: '/' },
       ],
+      buyItemDrop: [
+        { title: '디아블로2', route: '/'},
+        { title: '바람의나라', route: '/'},
+        { title: '아이온', route: '/'},
+        { title: '던전엔파이터', route: '/'},
+      ],
+      sellItemDrop: [
+        { title: '디아블로2', route: '/'},
+        { title: '바람의나라', route: '/'},
+        { title: '아이온', route: '/'},
+        { title: '던전엔파이터', route: '/'},
+      ]
+
     }
   },
   methods: {
