349e4c417abb1972834f033071dcf6e47f7bfcbc
HEEJI YUN <yhj8147e@gmail.com>
2022-11-14 17:54:58 +0900

    feat: [윤희지] 게시물 관리 시스템을 위한 entity, repository 구현 완료 [HOME-ALONE5]

--- p4th/HeejiYun/backend/src/main/java/com/example/backend/entity/board/Board.java
+++ p4th/HeejiYun/backend/src/main/java/com/example/backend/entity/board/Board.java
@@ -1,8 +1,7 @@
 package com.example.backend.entity.board;
 
 import com.example.backend.entity.member.Member;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
--- p4th/HeejiYun/backend/src/main/java/com/example/backend/entity/board/BoardCategory.java
+++ p4th/HeejiYun/backend/src/main/java/com/example/backend/entity/board/BoardCategory.java
@@ -1,13 +1,14 @@
 package com.example.backend.entity.board;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@ToString(exclude = "boards")
 @Entity
 @NoArgsConstructor
 public class BoardCategory {
