67e1eed08a6fb470f906e2f1b0a30767f0047b0a
parkseonho1 <preferr1@naver.com>
2022-11-16 01:10:16 +0900

    feat: [박선호] 로그인 후 연결되는 메인페이지 UI [LOOK-STYLE5]

--- p4th/SeonhoPark/flutter/look_style/lib/pages/main_page.dart
+++ p4th/SeonhoPark/flutter/look_style/lib/pages/main_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:look_style/pages/sign_up_page.dart';
+import 'package:look_style/pages/board_register_page.dart';
 import 'package:look_style/utility/custom_icons_icons.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+  MainPage({Key? key, this.email, this.nickname}) : super(key: key);
+
+  String? email;
+  String? nickname;
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -15,58 +18,19 @@ class _MainPageState extends State<MainPage> {
   int _selectedPageIndex = 0;
 
   List _pages = [
-    Text("Fashion", style: TextStyle(fontSize: 30)),
-    Text("Hair Style", style: TextStyle(fontSize: 30)),
-    Text("Make-up", style: TextStyle(fontSize: 30)),
-    Text("MyPage", style: TextStyle(fontSize: 30))
+    Text('1'),
+    Text('2'),
+    Text('3'),
+    Text('4'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            SizedBox(height: 20),
-            Center(
-                child: Text("새 게시물 등록", style: TextStyle(fontSize: 30,))
-            ),
-            SizedBox(height: 20),
-            ListTile(
-              leading: Icon(CustomIcons.outfit),
-              title: Text('Fashion'),
-              onTap: () {Get.back();},
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(CustomIcons.salon),
-              title: Text('Hair Style'),
-              onTap: () {},
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(CustomIcons.makeup),
-              title: Text('Make-up'),
-              onTap: () {},
-              trailing: Icon(Icons.add),
-            ),
-          ],
-        ),
-      ),
       appBar: AppBar(
-          // title: Image.asset('assets/logo.png', fit: BoxFit.fitHeight,),
-          // leading: IconButton(
-          //     onPressed: (){},
-          //     icon: Icon(Icons.menu)
-          // ),
-          leading: Builder(
-            builder: (context) {
-              return IconButton(
-                onPressed: () => Scaffold.of(context).openDrawer(),
-                icon: Icon(CustomIcons.add_in_square),
-              );
-            }
+          leading: IconButton(
+                onPressed: () {},
+                icon: Icon(CustomIcons.add_square),
           ),
           title: Text(
             "Look Style",
@@ -87,9 +51,7 @@ class _MainPageState extends State<MainPage> {
             ),
           ],
       ),
-      body: Center(
-          child: _pages[_selectedPageIndex]
-      ),
+      body: _pages[_selectedPageIndex],
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           color: Colors.white10,
@@ -104,10 +66,10 @@ class _MainPageState extends State<MainPage> {
           onTap: _onItemTapped,
           currentIndex: _selectedPageIndex,
           items: [
-            BottomNavigationBarItem(icon: Icon(CustomIcons.outfit), label: 'Fashion'),
-            BottomNavigationBarItem(icon: Icon(CustomIcons.salon), label: 'Hair Style'),
-            BottomNavigationBarItem(icon: Icon(CustomIcons.makeup), label: 'Make-up'),
-            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'My Page')
+            BottomNavigationBarItem(icon: Icon(CustomIcons.outfit), label: '패션'),
+            BottomNavigationBarItem(icon: Icon(CustomIcons.salon), label: '헤어스타일'),
+            BottomNavigationBarItem(icon: Icon(CustomIcons.makeup), label: '메이크업'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: '마이페이지')
           ],
         ),
       ),
