0f12ccfc39c2410131865c39e41710f708292780
choijaemin <foertonw5@naver.com>
2022-11-16 00:34:31 +0900

    feat :[최재민] 비밀번호 일치를 확인 하기 위한 passwordCheckTextForm 생성 [Learning-Helper5]

---
+++ p4th/JaeminChoi/flutter/lib/components/text_form_filed/password_checked_text_form_field.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../utility/decorations/text_form_decoration.dart';
+
+class PasswordCheckedTextFormField extends StatefulWidget{
+  const PasswordCheckedTextFormField({Key?key ,required this.widthSize, required this.controller}) : super(key:key);
+
+  final TextEditingController controller;
+  final double widthSize;
+
+  @override
+  State<PasswordCheckedTextFormField> createState() => _PasswordCheckedTextFormField();
+}
+
+class _PasswordCheckedTextFormField extends State<PasswordCheckedTextFormField>{
+
+  late FocusNode _PasswordCheckedFocus;
+  var controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = widget.controller;
+    _PasswordCheckedFocus = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _PasswordCheckedFocus.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.widthSize,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: controller,
+            decoration: textFormDecoration("check Password"),
+            obscureText:true,
+            style: const TextStyle(color: Colors.black),
+            keyboardType: TextInputType.visiblePassword,
+            focusNode: _PasswordCheckedFocus,
+            autovalidateMode : AutovalidateMode.onUserInteraction ,
+            onSaved: (value) {
+              setState(()
+              {controller.text = value!;}
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
