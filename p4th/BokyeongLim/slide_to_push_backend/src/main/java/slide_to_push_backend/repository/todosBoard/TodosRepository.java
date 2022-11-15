fdf974aaa6733acbef4ae0cb84be053d71953900
tonowoods <tono.woods@gmail.com>
2022-11-15 16:23:50 +0900

    feat: 확인된 사용자가 등록한 리스트 정보를 찾기 위한 쿼리문 추가 및 엔티티 수정 [SLIDE-TO-PUSH26]

--- p4th/BokyeongLim/slide_to_push_backend/src/main/java/slide_to_push_backend/repository/todosBoard/TodosRepository.java
+++ p4th/BokyeongLim/slide_to_push_backend/src/main/java/slide_to_push_backend/repository/todosBoard/TodosRepository.java
@@ -2,11 +2,16 @@ package slide_to_push_backend.repository.todosBoard;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import slide_to_push_backend.entity.member.Account;
 import slide_to_push_backend.entity.todosBoard.Todos;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TodosRepository extends JpaRepository<Todos, Long> {
     @Query("select t from Todos t where t.boardNo = :boardNo")
     Optional<Todos> findByBoardNo(Long boardNo);
+
+    @Query("select t from Todos t where t.account = :account")
+    List<Todos> findByAccount(Account account);
 }
