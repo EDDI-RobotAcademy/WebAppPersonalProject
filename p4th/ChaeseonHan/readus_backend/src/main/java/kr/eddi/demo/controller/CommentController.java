1a348dd5c6b83de4a916b2b965faad19126f4912
Hanchaeseon <mdmodina34@gmail.com>
2022-11-16 21:04:29 +0900

    feat.[한채선] 프론트로부터 댓글 작성 요청 받을 controller [READUS-32]

---
+++ p4th/ChaeseonHan/readus_backend/src/main/java/kr/eddi/demo/controller/CommentController.java
@@ -0,0 +1,25 @@
+package kr.eddi.demo.controller;
+
+import kr.eddi.demo.controller.form.CommentToBoardForm;
+import kr.eddi.demo.service.CommentServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/comment")
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
+public class CommentController {
+
+    @Autowired
+    CommentServiceImpl service;
+
+    @PostMapping("/write/{boardNo}")
+    public Boolean writeComment(@PathVariable("boardNo") Long boardNo,
+                                @RequestBody CommentToBoardForm commentToBoardForm) {
+        return service.write(commentToBoardForm.toCommentToBoardWriteRequest());
+    }
+
+}
