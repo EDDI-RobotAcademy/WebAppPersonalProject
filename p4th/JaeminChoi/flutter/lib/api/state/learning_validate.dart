49869d518535b9215235c9d38f93153f95fa996f
choijaemin <foertonw5@naver.com>
2022-11-17 22:29:37 +0900

    feat :[최재민] 관리자가 학습 등록을 하기 위한 Form UI[Learning-Helper28]

---
+++ p4th/JaeminChoi/flutter/lib/api/state/learning_validate.dart
@@ -0,0 +1,4 @@
+class LearningValidate{
+  static bool isWordItemRegister = false;
+
+}
--- p4th/JaeminChoi/flutter/lib/utility/decorations/text_form_decoration.dart
+++ p4th/JaeminChoi/flutter/lib/utility/decorations/text_form_decoration.dart
@@ -25,3 +25,28 @@ InputDecoration textFormDecoration(String hintText) {
         borderSide: const BorderSide(color: ColorStyle.focusedTextForm, width: 2.0)),
     );
 }
+
+
+InputDecoration textFormDecorationForAdmin(String hintText) {
+  return InputDecoration(
+    filled: true,
+    fillColor: ColorStyle.textFormGray,
+    hintStyle : defaultTextFieldTextStyle(),
+    hintText: hintText,
+    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+    enabledBorder:
+    OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: ColorStyle.lineGray, width: 2.0)
+    ),
+    focusedBorder:
+    OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: ColorStyle.focusedTextForm, width: 2.0)),
+    errorBorder:
+    OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: ColorStyle.errorTextForm , width: 2.0)),
+    focusedErrorBorder:
+    OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: ColorStyle.focusedTextForm, width: 2.0)),
+  );
+}
