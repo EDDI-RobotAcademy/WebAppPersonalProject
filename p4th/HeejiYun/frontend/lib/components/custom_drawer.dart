b540865344769f7cf297fcd79acbc9380b823b70
HEEJI YUN <yhj8147e@gmail.com>
2022-11-17 21:35:58 +0900

    feat: [윤희지] 로그인 상태에 따라 다르게 보이는 메뉴 UI [HOME-ALONE14]

--- p4th/HeejiYun/frontend/lib/components/custom_drawer.dart
+++ p4th/HeejiYun/frontend/lib/components/custom_drawer.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/components/custom_rich_text.dart';
 import 'package:frontend/utility/main_color.dart';
+
+import 'forms/board_register_form.dart';
 
 
 class CustomDrawer extends StatelessWidget {
@@ -11,83 +12,114 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String email = "tmp@test.com";
+    String nickname = "경수";
+
     debugPrint(isLogin.toString());
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          Visibility(
-              visible: !isLogin,
-                  child: DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: MainColor.mainColor,
-                    ),
-                    child: RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: '로그인',
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.pushNamed(context, "/sign-in");
-                              }),
-                      ]),
-                    ),
+    if (isLogin == true) {
+      return _logInDrawer(context, email, nickname);
+    }  else {
+       return _logOutDrawer(context);
+     }
+  }
+    Widget _logInDrawer(BuildContext context, String email, String nickname) {
+      return Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text(email),
+                accountEmail: Text(nickname),
+              ),
+              ListTile(
+                title: CustomRichText(text: "홈화면 가기", route: "/home"),
+              ),
+              ExpansionTile(
+                title: const Text('게시판 보기'),
+                initiallyExpanded: false,
+                children: <Widget>[
+                  Divider(height: 10,),
+                  Container (
+                      height: 30,
+                      width: 200,
+                      child: CustomRichText(text: "자유게시판", route: "/board-list-free")
                   ),
-                ),
-          Visibility(
-              visible: isLogin,
-              child: UserAccountsDrawerHeader(
-                accountName: Text("영희"),
-                accountEmail: Text("ddd@gmail.com"),
-            )
-          ),
-          ExpansionTile(
-            title: const Text('게시판 보기'),
-            initiallyExpanded: false,
+                  Divider(height: 3,),
+                  Container (
+                      height: 30,
+                      width: 200,
+                      child: CustomRichText(text: "자취인 질문 게시판", route: "/board-list-ask")
+                  ),
+                  Divider(height: 3,),
+                  Container (
+                      height: 30,
+                      width: 200,
+                      child: CustomRichText(text: "1인분 레시피 게시판", route: "/board-list-recipe")
+                  )
+                ],
+              ),
+              ListTile(
+                title: Text("게시물 작성하기"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                      builder: (context) => BoardRegisterForm(),
+                  ));
+                }
+              ),
+                  ListTile(
+                    title: CustomRichText(text: "내 정보 보기", route: "/my-page")
+                  ),
+                  ListTile(
+                    title: CustomRichText(text: "공지사항", route: "/board-list-notice"),
+                  ),
+            ],
+          )
+      );
+    }
+
+  Widget _logOutDrawer(BuildContext context) {
+    return Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                child: CustomRichText(text: "로그인", route: "/sign-in"),
+                decoration: BoxDecoration(
+                  color: MainColor.mainColor
+                )
+            ),
+            ListTile(
+              title: CustomRichText(text: "홈화면 가기", route: "/home"),
+            ),
+            ExpansionTile(
+              title: const Text('게시판 보기'),
+              initiallyExpanded: false,
               children: <Widget>[
                 Divider(height: 10,),
                 Container (
-                  height: 30,
-                  width: 200,
-                  child: CustomRichText(text: "자유게시판", route: "/board-list-stls")
-                  ),
-                Divider(height: 3,),
-                Container (
                     height: 30,
                     width: 200,
-                    child: Text("자취인 질문 게시판")
+                    child: CustomRichText(text: "자유게시판", route: "/board-list-free")
                 ),
                 Divider(height: 3,),
                 Container (
                     height: 30,
                     width: 200,
-                    child: Text("1인분 레시피 공유")
+                    child: CustomRichText(text: "자취인 질문 게시판", route: "/board-list-ask")
+                ),
+                Divider(height: 3,),
+                Container (
+                    height: 30,
+                    width: 200,
+                    child: CustomRichText(text: "1인분 레시피 게시판", route: "/board-list-recipe")
                 )
               ],
-          ),
-          Visibility(
-              visible: isLogin,
-              child: ListTile(
-                title: const Text('나의 정보'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              )
             ),
-          ListTile(
-            title: const Text('홈 화면 이동'),
-            onTap: () {
-              Navigator.pushNamed(context, "/home");
-            },
-          ),
-          ListTile(
-            title: const Text('QnA'),
-            onTap: () {
-              Navigator.pushNamed(context, "/home");
-            },
-          )
-        ],
-      ),
+            ListTile(
+              title: CustomRichText(text: "공지사항", route: "/board-list-notice"),
+            ),
+          ],
+        )
     );
   }
 }
