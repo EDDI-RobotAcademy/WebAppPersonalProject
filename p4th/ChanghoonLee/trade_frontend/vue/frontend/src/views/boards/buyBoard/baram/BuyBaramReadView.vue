69d211c6a114889f5fb3aed45dbe8fab435c8989
kookiki91 <kookiki91@naver.com>
2022-11-17 22:09:47 +0900

    fix:[이창훈] 게시글 삭제 시 삭제여부 확인하는 모달창 나타나도록 코드 수정 [TRADE-ITEM-26]

--- p4th/ChanghoonLee/trade_frontend/vue/frontend/src/views/boards/buyBoard/baram/BuyBaramReadView.vue
+++ p4th/ChanghoonLee/trade_frontend/vue/frontend/src/views/boards/buyBoard/baram/BuyBaramReadView.vue
@@ -13,7 +13,31 @@
         <router-link :to="{ name: 'BuyBaramModifyView', params: { buyBaramNo } }">
           <v-btn class="grey white--text" rounded depressed small>게시물 수정</v-btn>&nbsp;
         </router-link>
-        <v-btn class="grey white--text" rounded depressed small @click="onDelete">삭제</v-btn>&nbsp;
+        <v-dialog v-model="deleteDialog" persisten max-width="400">
+          <template v-slot:activator="{on}">
+            <v-btn class="grey white--text" rounded depressed small v-on="on">
+              삭제
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="headline">
+              {{ deleteTitle }}
+            </v-card-title>
+            <v-card-text>
+              삭제 하시겠습니까?
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="grey darken-2" text @click="cancelBtn">
+                취소
+              </v-btn>
+              <v-btn color="red" text @click="onDelete">
+                삭제
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        &nbsp;
         <router-link :to="{ name: 'BuyBaramListView' }">
           <v-btn class="grey white--text" rounded depressed small>돌아가기</v-btn>
         </router-link>
@@ -35,6 +59,12 @@ import ToolbarForm from "@/components/toolbar/ToolbarForm";
 
 export default {
   name: "BuyBaramReadView",
+  data() {
+    return {
+      deleteDialog: false,
+      deleteTitle:"게시글 삭제"
+    }
+  },
   components: {
     BuyBaramRead,
     TopMenuForm,
@@ -65,7 +95,10 @@ export default {
   },
   created() {
     this.requestBuyBaramFromSpring(this.buyBaramNo)
-  }
+  },
+  cancelBtn() {
+    this.deleteDialog = false
+  },
 }
 </script>
 
