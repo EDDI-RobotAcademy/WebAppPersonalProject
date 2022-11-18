0c9d51ae764a88448b1b10476fb4951416d0600a
tonowoods <tono.woods@gmail.com>
2022-11-18 21:30:08 +0900

    feat: 컨트롤러를 통해 받은 토큰으로 사용자 식별 후 사용자가 가진 친구들의 리스트를 만들어 반환하는 시스템[SLIDE-TO-PUSH32]

--- p4th/BokyeongLim/slide_to_push_backend/src/test/java/slide_to_push_backend/FriendsEntityTest.java
+++ p4th/BokyeongLim/slide_to_push_backend/src/test/java/slide_to_push_backend/FriendsEntityTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import slide_to_push_backend.entity.friend.Friend;
 import slide_to_push_backend.entity.member.Account;
-import slide_to_push_backend.repository.friend.FriendRepositoy;
+import slide_to_push_backend.repository.friend.FriendRepository;
 import slide_to_push_backend.repository.member.AccountRepository;
 
 import java.util.Optional;
@@ -14,7 +14,7 @@ import java.util.Optional;
 public class FriendsEntityTest {
 
     @Autowired
-    FriendRepositoy repository;
+    FriendRepository repository;
 
     @Autowired
     AccountRepository accountRepository;
@@ -23,8 +23,9 @@ public class FriendsEntityTest {
     void testCreateFriendTable () {
 
         Long myId = 2L;
-        Long friendId = 11L;
-        Boolean isFriended = true;
+        Long friendId = 20L;
+
+
 
         Optional<Account> maybeMember = accountRepository.findByAccount(myId);
         Account myAccount = maybeMember.get();
@@ -32,14 +33,11 @@ public class FriendsEntityTest {
         Optional<Account> maybeMember2 = accountRepository.findByAccount(friendId);
         Account friendedAccount = maybeMember2.get();
 
-        if(isFriended) {
-            Friend friend = new Friend(myAccount, friendedAccount, true);
-            Friend friend2 = new Friend(friendedAccount, myAccount, true);
+        Friend friend = new Friend(myAccount, friendedAccount);
+        Friend friend2 = new Friend(friendedAccount, myAccount);
 
-            repository.save(friend);
-            repository.save(friend2);
-        }
-
+        repository.save(friend);
+        repository.save(friend2);
 
     }
 }
--- p4th/BokyeongLim/slide_to_push_frontend/test/widget_test.dart
+++ p4th/BokyeongLim/slide_to_push_frontend/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:my_todos/main.dart';
 
-import 'package:slide_to_push_frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
