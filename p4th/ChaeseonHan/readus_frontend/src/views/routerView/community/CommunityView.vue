2c8a6dbba70b34b8be11084c72d4774af8576f2e
Hanchaeseon <mdmodina34@gmail.com>
2022-11-18 21:12:33 +0900

    fix.[한채선] 분리한 카테고리 버튼과 검색/글쓰기 컴포넌트들 연결 [READUS-18]

--- p4th/ChaeseonHan/readus_frontend/src/views/routerView/community/CommunityView.vue
+++ p4th/ChaeseonHan/readus_frontend/src/views/routerView/community/CommunityView.vue
@@ -1,22 +1,28 @@
 <template>
   <v-container>
     <!-- TODO: 2022.11.11 후에 pageble 이용해서 수정해야함 -->
-
+    <common-community-category-buttons/>
     <common-board-list :board-list="this.$store.state.communityMainBoardList"/>
+    <common-search-and-write-buttons/>
   </v-container>
 </template>
 
 <script>
 import CommonBoardList from "@/components/common/CommonBoardList";
 import {mapActions} from "vuex";
+import CommonSearchAndWriteButtons from "@/components/common/CommonSearchAndWriteButtons";
+import CommonCommunityCategoryButtons from "@/components/common/CommonCommunityCategoryButtons";
 export default {
   name: "CommunityView",
-  components: { CommonBoardList },
-  computed: {
-  },
+  components: { CommonCommunityCategoryButtons, CommonSearchAndWriteButtons, CommonBoardList },
+  data() {
+    return {
 
+    }
+  },
   methods: {
-    ...mapActions(['requestAllCommunityBoardListTOSpring'])
+    ...mapActions(['requestAllCommunityBoardListTOSpring']),
+
   },
 
   mounted() {
