282c1d855bd08cfe1de58a6af4799a330a96044b
Karam Kim <kim.ram454@gmail.com>
2022-11-21 03:28:31 +0900

    Feat: [김가람] F) 공통으로 사용할 수 있는 게시판 리스트 컴포넌트 [PLANTS26]

---
+++ p4th/KaramKim/vue/plants_web/src/components/board/BoardListForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="mt-10 mb-10">
+            <div
+                style="
+                font-size: 30px;
+                text-align: center;
+                font-weight: bold;
+                margin-bottom: 10px;
+              "
+                class="light-green--text text--darken-1"
+            >
+              <p>{{ boardName }}</p>
+            </div>
+            <div
+                style="
+                font-size: 20px;
+                text-align: center;
+                font-weight: bold;
+                color: grey;
+              "
+            >
+              {{ boardDescription }}
+            </div>
+          </div>
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-data-table
+              :headers="headers"
+              :items="items"
+              :items-per-page="5"
+              @click:row="rowClick"
+              class="elevation-1"
+              :search="search"
+          >
+            <template v-slot:top>
+              <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Search"
+                  class="mx-4"
+                  single-line
+              ></v-text-field>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "BoardListForm",
+  data() {
+    return {
+      search: '',
+    }
+  },
+  props: {
+    boardName: String,
+    boardDescription: String,
+    headers: Array,
+    items: Array,
+    boards: Array,
+  },
+  methods: {
+    rowClick(row) {
+      console.log(row.boardNo)
+      this.$router.push({ name: 'BoardReadView',
+      params: {boardNo: row.boardNo.toString()}})
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
