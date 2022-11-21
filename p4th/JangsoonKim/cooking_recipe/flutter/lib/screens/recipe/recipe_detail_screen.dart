af2f03bd4500d17228aabd2f6771452a38de78a1
jssnk <ssnssnl@kakao.com>
2022-11-21 18:39:14 +0900

    feat: [김장순] (수정) 레시피 리스트 중 하나를 클릭시 작성된 레시피의 세부사항을 확인할 수 있는 레시피 상세보기 화면 UI (+수정, 삭제 버튼) [COOKING-RECIPE29]

---
+++ p4th/JangsoonKim/cooking_recipe/flutter/lib/screens/recipe/recipe_detail_screen.dart
@@ -0,0 +1,67 @@
+import 'package:demo/widgets/drawer/custom_drawer.dart';
+import 'package:demo/widgets/recipe/recipe_detail_form.dart';
+import 'package:flutter/material.dart';
+
+import '../../utilities/spring_recipe_api.dart';
+
+class RecipeDetailScreen extends StatefulWidget {
+  final dynamic recipe;
+  final int recipeNo;
+
+  const RecipeDetailScreen(
+      {Key? key, required this.recipe, required this.recipeNo})
+      : super(key: key);
+
+  @override
+  State<RecipeDetailScreen> createState() => RecipeDetailScreenState();
+}
+
+class RecipeDetailScreenState extends State<RecipeDetailScreen> {
+  late Future<dynamic> _future;
+  late List<dynamic> imageList;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _future = getImages();
+  }
+
+  Future getImages() async {
+    await SpringRecipeApi().loadImages(widget.recipeNo).then((imgList) {
+      setState(() {
+        imageList = imgList!;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        endDrawer: CustomDrawer(),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          foregroundColor: Colors.black,
+          centerTitle: false,
+        ),
+        body: FutureBuilder(
+            future: _future,
+            builder: ((context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(snapshot.error.toString()),
+                  );
+                } else {
+                  return RecipeDetailForm(
+                      imageList: imageList, recipe: widget.recipe);
+                }
+              } else {
+                return const Text("망");
+              }
+            })));
+  }
+}
--- p4th/JangsoonKim/cooking_recipe/flutter/lib/widgets/recipe/recipe_detail_form.dart
+++ p4th/JangsoonKim/cooking_recipe/flutter/lib/widgets/recipe/recipe_detail_form.dart
@@ -1,12 +1,21 @@
+import 'package:demo/screens/recipe/recipe_modify_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../app_theme.dart';
-import '../../model/tmp_recipe.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class RecipeDetailForm extends StatefulWidget {
-  final Recipe recipe;
+import '../../utilities/spring_recipe_api.dart';
+import '../screen_controller.dart';
 
-  const RecipeDetailForm({Key? key, required this.recipe}) : super(key: key);
+class RecipeDetailForm extends StatefulWidget {
+  final dynamic recipe;
+  final List<dynamic> imageList;
+  List<String> tmpList = [];
+
+  RecipeDetailForm({Key? key, required this.recipe, required this.imageList})
+      : super(key: key);
 
   @override
   State<RecipeDetailForm> createState() => _RecipeDetailFormState();
@@ -14,20 +23,36 @@ class RecipeDetailForm extends StatefulWidget {
 
 class _RecipeDetailFormState extends State<RecipeDetailForm> {
   List<Widget> carouseImages = [];
-  bool _like = false;
+
+  //bool _like = false;
   int _current = 0;
   double _rate = 0.0;
+  bool tmp = false;
+  String nickname = '';
 
   @override
   void initState() {
     // TODO: implement initState
+    Future.delayed(Duration.zero, () async {
+      asyncMethod();
+    });
     super.initState();
-    _like = widget.recipe.like;
+    //_like = widget.recipe.like;
     _rate = widget.recipe.rating;
 
-    for (var i = 0; i < widget.recipe.images.length; i++) {
-      carouseImages!.add(Image.asset(widget.recipe.images[i]));
+    for (var i = 0; i < widget.imageList.length; i++) {
+      carouseImages!.add(Image.asset(
+          "assets/images/uploadImgs/${widget.imageList[i].imagePath}"));
+      widget.tmpList!
+          .add("assets/images/uploadImgs/${widget.imageList[i].imagePath}");
     }
+  }
+
+  asyncMethod() async {
+    var any = await SharedPreferences.getInstance();
+    setState(() {
+      nickname = any.getString("userNickname") ?? "없다";
+    });
   }
 
   @override
@@ -57,11 +82,11 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                         reverse: false,
                         autoPlay: false,
                       ),
-                      items: widget.recipe.images.map((path) {
+                      items: widget.tmpList.map((name) {
                         return Builder(
                           builder: (BuildContext context) {
                             return Image.asset(
-                              path,
+                              name,
                               width: size.width,
                               fit: BoxFit.cover,
                             );
@@ -84,7 +109,7 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                           return Container(
                             width: 8,
                             height: 10,
-                            margin: EdgeInsets.symmetric(
+                            margin: const EdgeInsets.symmetric(
                                 vertical: 10, horizontal: 4),
                             decoration: BoxDecoration(
                               shape: BoxShape.circle,
@@ -105,7 +130,7 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                   ],
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+                  padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
                   child: Row(
                     children: [
                       SizedBox(
@@ -114,10 +139,33 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                       Expanded(
                         child: Text(
                           widget.recipe.title,
-                          style: TextStyle(
+                          style: const TextStyle(
                               fontSize: 24, fontWeight: FontWeight.w600),
                         ),
-                      )
+                      ),
+                      const Spacer(flex: 1),
+                      /*_like*/ tmp == true
+                          ? IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  /*_like*/ tmp = false;
+                                });
+                              },
+                              icon: const Icon(
+                                Icons.favorite,
+                                color: Colors.redAccent,
+                              ))
+                          : IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  /*_like*/ tmp = true;
+                                });
+                              },
+                              icon: const Icon(
+                                Icons.favorite_outline,
+                                color: Colors.redAccent,
+                                size: 30,
+                              ))
                     ],
                   ),
                 ),
@@ -126,12 +174,12 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                     Padding(
                       padding: const EdgeInsets.only(left: 25.0),
                       child: Text(
-                        widget.recipe.foodCategory,
-                        style: TextStyle(
+                        widget.recipe.category,
+                        style: const TextStyle(
                             fontSize: 18, fontWeight: FontWeight.w300),
                       ),
                     ),
-                    Spacer(
+                    const Spacer(
                       flex: 1,
                     ),
                     _rate == 0
@@ -139,7 +187,7 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                             padding: const EdgeInsets.only(right: 15),
                             child: TextButton(
                                 onPressed: () {},
-                                child: Text(
+                                child: const Text(
                                   "레시피를 평가해주세요",
                                   style: TextStyle(color: Colors.red),
                                 )),
@@ -148,14 +196,14 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                             children: [
                               Text(
                                 "${_rate}",
-                                style: TextStyle(
+                                style: const TextStyle(
                                     fontSize: 19, fontWeight: FontWeight.w400),
                               ),
                               Padding(
                                 padding: const EdgeInsets.only(right: 15),
                                 child: IconButton(
                                     onPressed: () {},
-                                    icon: Icon(
+                                    icon: const Icon(
                                       Icons.star,
                                       color: Colors.amberAccent,
                                       size: 33,
@@ -165,49 +213,81 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                           )
                   ],
                 ),
+                Container(
+                  height: 1,
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  color: Colors.grey.withOpacity(0.3),
+                ),
                 Padding(
                   padding:
                       const EdgeInsets.only(left: 25, right: 20, bottom: 15),
                   child: Row(
                     children: [
-                      Column(
-                          children: [
-                            CircleAvatar(
-                              radius: 50,
-                              backgroundImage: AssetImage("assets/icons/profile.png"),
-                              backgroundColor: Colors.white,
-                            ),
-                            Text(widget.recipe.nickname,
-                                style: TextStyle(
-                                    fontSize: 19, fontWeight: FontWeight.bold)),
-                            SizedBox(
-                              height: size.height * 0.03,
-                            )
-                          ]
+                      Column(children: [
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage("assets/icons/profile.png"),
+                          backgroundColor: Colors.white,
+                        ),
+                        Text(widget.recipe.writer,
+                            style: const TextStyle(
+                                fontSize: 19, fontWeight: FontWeight.bold)),
+                        SizedBox(
+                          height: size.height * 0.03,
+                        )
+                      ]),
+                      Spacer(
+                        flex: 1,
                       ),
-                      Spacer(flex: 1),
-                      _like == true
-                          ? IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  _like = false;
-                                });
-                              },
-                              icon: Icon(
-                                Icons.favorite,
-                                color: Colors.redAccent,
-                              ))
-                          : IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  _like = true;
-                                });
-                              },
-                              icon: Icon(
-                                Icons.favorite_outline,
-                                color: Colors.redAccent,
-                                size: 30,
-                              ))
+                      Column(
+                        children: [
+                          nickname == widget.recipe.writer
+                              ? Row(
+                                  children: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Get.to(RecipeModifyScreen(
+                                            tmpList: widget.tmpList,
+                                            carouseImages: carouseImages,
+                                            recipe: widget.recipe,
+                                          ));
+                                        },
+                                        child: const Text(
+                                          "수정",
+                                          style: TextStyle(color: Colors.grey),
+                                        )),
+                                    TextButton(
+                                        onPressed: () {
+                                          Future<void> future =
+                                              SpringRecipeApi().recipeDelete(
+                                                  widget.recipe.recipeNo);
+                                          future.then((value) {
+                                            print("레시피 삭제 성공");
+                                            _showDeleteSuccessDialog(
+                                                title: "성공",
+                                                content: "레시피 삭제 성공했습니다.",
+                                                buttonText: "확인");
+                                          }).catchError((error) {
+                                            print("레시피 삭제 실패");
+                                            _showDeleteFailDialog(
+                                                title: "실패",
+                                                content: "레시피 삭제 실패했습니다. 통신상태를 확인해주세요.",
+                                                buttonText: "확인");
+                                          });
+                                        },
+                                        child: const Text("삭제",
+                                            style:
+                                                TextStyle(color: Colors.grey)))
+                                  ],
+                                )
+                              : Text(''),
+                          Text(
+                            widget.recipe.regDate,
+                            style: const TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
@@ -220,9 +300,10 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                   height: size.height * 0.05,
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 25, right: 20, bottom: 15),
+                  padding:
+                      const EdgeInsets.only(left: 25, right: 20, bottom: 15),
                   child: Row(
-                    children: [
+                    children: const [
                       Text(
                         '조리 순서',
                         textAlign: TextAlign.left,
@@ -230,13 +311,14 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                             fontWeight: FontWeight.w600,
                             fontSize: 22,
                             letterSpacing: 0.27,
-                            color: AppTheme.indiaInk
-                        ),
+                            color: AppTheme.indiaInk),
                       )
                     ],
                   ),
                 ),
-                SizedBox(height: size.height * 0.03,),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
                 Padding(
                   padding:
                       const EdgeInsets.only(left: 25, right: 20, bottom: 15),
@@ -244,12 +326,53 @@ class _RecipeDetailFormState extends State<RecipeDetailForm> {
                     widget.recipe.content,
                     style: TextStyle(fontSize: 18),
                   ),
-                )
+                ),
               ],
             ),
           ],
         ),
       ),
     );
+  }
+  void _showDeleteFailDialog(
+      {String? title, String? content, String? buttonText}) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text(title!),
+            content: Text(content!),
+            actions: [
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text(buttonText!),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  void _showDeleteSuccessDialog(
+      {String? title, String? content, String? buttonText}) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text(title!),
+            content: Text(content!),
+            actions: [
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text(buttonText!),
+                onPressed: () {
+                  Get.off(ScreenController());
+                },
+              )
+            ],
+          );
+        });
   }
 }
