c1571ee82edb6a357ab9b5017a11ce39be4002b3
soyeonkim96 <shimmer326@gmail.com>
2022-11-22 16:45:31 +0900

    feat: [김소연] 로그인한 사용자가 후기게시판에 게시글을 등록하기 위한 게시글 작성UI[LOOKLEND24]

---
+++ p4th/SoyeonKim/frontend/looklend/src/views/boards/reviewBoards/ReviewRegisterView.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <header-view-practice/>
+
+    <br/><br/><br/><br/><br/>
+
+    <v-row>
+      <review-register @submit="onSubmit"/>
+    </v-row>
+
+    <br/>
+    <br/>
+    <br/>
+
+    <FooterForm/>
+  </nav>
+</template>
+
+<script>
+import ReviewRegister from "@/components/boards/reviewBoards/ReviewRegister";
+import HeaderViewPractice from "@/views/main/HeaderViewPractice";
+import FooterForm from "@/components/common/FooterForm";
+import {mapActions} from "vuex";
+export default {
+  name: "ReviewRegisterView",
+  components: {FooterForm, HeaderViewPractice, ReviewRegister},
+  methods: {
+    ...mapActions([
+      'requestRegisterReviewToSpring'
+    ]),
+    async onSubmit (payload) {
+      await this.requestRegisterReviewToSpring(payload)
+      await this.$router.push({
+        name: 'ReviewListView'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
