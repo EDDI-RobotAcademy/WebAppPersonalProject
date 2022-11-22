870866af63233247e51b3542644c88d971233063
choijaemin <foertonw5@naver.com>
2022-11-23 00:05:51 +0900

    feat :[최재민] 회원 가입 시 api 에서 결과를 debug 하고 signUpState 를 관리[Learning-Helper6]

--- p4th/JaeminChoi/flutter/lib/api/spring_sign_up_api.dart
+++ p4th/JaeminChoi/flutter/lib/api/spring_sign_up_api.dart
@@ -1,33 +1,38 @@
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:leaing_helper/api/state/sign_up_validate_state.dart';
 
 import 'info/sign_up_info.dart';
 
 
 class SpringSignUpApi {
-  static var response;
+  static var SignUpresponse;
 
   static const String httpUri = '192.168.0.8:7776';
 
   signUp (SignUpAccount account) async {
     var data = { 'email': account.email, 'password': account.password ,'nickName' :account.nickName };
     var body = json.encode(data);
-
     debugPrint(body);
+    try{
+      var SignUpresponse = await http.post(
+        Uri.http(httpUri, '/member/sign-up'),
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
 
-    var response = await http.post(
-      Uri.http(httpUri, '/member/sign-up'),
-      headers: {"Content-Type": "application/json"},
-      body: body,
-    );
+      if(SignUpresponse.statusCode == 200){
+        debugPrint("통신완료");
+        var result = jsonDecode(SignUpresponse.body);
+        debugPrint("result :" + result.toString());
 
-    if (response.statusCode == 200) {
-      debugPrint("통신 확인");
-      return true;
-    }else{
-      debugPrint("error");
-      return false;
+        if (result == true) {
+          SignUpValidateState.isSignUpCheck = true;
+        }
+      }
+    }catch(e){
+      debugPrint("오류 발생" + e.toString());
     }
   }
 }
