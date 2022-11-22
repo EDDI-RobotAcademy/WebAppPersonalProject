a724abe40f7fce36b9c87ab033fec6b5c247b536
choijaemin <foertonw5@naver.com>
2022-11-23 00:24:27 +0900

    feat :[최재민] 관리자 아이디로 로그인시 보여지는 메인페이지 UI[Learning-Helper28]

--- p4th/JaeminChoi/flutter/lib/pages/admin_pages/admin_main_page.dart
+++ p4th/JaeminChoi/flutter/lib/pages/admin_pages/admin_main_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:leaing_helper/utility/decorations/color.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-import '../../components/text_form_filed/admin_text_form.dart';
+import '../../components/app_bar/common_app_bar.dart';
+import '../../components/app_bar/common_appbar_drawer.dart';
 import '../../utility/common_btn/text_btn_box.dart';
+import '../../utility/size.dart';
+import '../common_pages/main_page.dart';
+
 
 class AdminMainPage extends StatefulWidget{
   const AdminMainPage({Key? key}) :super (key:key);
@@ -12,30 +16,80 @@ class AdminMainPage extends StatefulWidget{
 }
 
 class _AdminMainPage extends State<AdminMainPage>{
+
+  static final storage = FlutterSecureStorage(); //
+  dynamic userTokenInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _asyncMethod();
+    });
+  }
+
+  _asyncMethod() async {
+    userTokenInfo = await storage.read(key:'sign_in');
+    debugPrint("userTokenInfo in adminPage = " + userTokenInfo);
+  }
+
+  void logout() async {
+    await storage.delete(key: 'sign_in');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: CommonAppBar( appBar: AppBar()),
+      drawer: const CommonAppbarDrawer(),
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.only(top: 60,left: 45),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment : CrossAxisAlignment.center,
-            children: const[
-              Text("환영 합니다 , 관리자님",style: TextStyle(fontSize: 17),),
-              Text("원하시는 관리 항목을 선택 해주세요",style: TextStyle(fontSize: 17)),
-              SizedBox(height: 30,),
-              Text("학습 등록",style: TextStyle(fontSize: 17)),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '단어 학습 등록 페이지로 이동',),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '문법 학습 등록 페이지로 이동',),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '독해 학습 등록 페이지로 이동',),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '논리 학습 등록 페이지로 이동',),
-              SizedBox(height: 30,),
-              Text("학습 수정 및 삭제",style: TextStyle(fontSize: 17)),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '단어 수정 및 삭제 페이지로 이동',),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '문법 수정 및 삭제 페이지로 이동',),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '독해 수정 및 삭제 페이지로 이동',),
-              TextBtnBoxRouter(router: '/learning_register_page', text: '논리 수정 및 삭제 페이지로 이동',),
+            children: [
+              const Text("환영 합니다 , 관리자님",style: TextStyle(fontSize: 17),),
+              const Text("원하시는 관리 항목을 선택 해주세요",style: TextStyle(fontSize: 17)),
+              const SizedBox(height: 30,),
+              const Text("학습 등록",style: TextStyle(fontSize: 17)),
+              const TextBtnBoxRouter(router: '/learning_register_page', text: '단어 학습 등록 페이지로 이동',),
+              const TextBtnBoxRouter(router: '/learning_register_page', text: '문법 학습 등록 페이지로 이동',),
+              const TextBtnBoxRouter(router: '/learning_register_page', text: '독해 학습 등록 페이지로 이동',),
+              const TextBtnBoxRouter(router: '/learning_register_page', text: '논리 학습 등록 페이지로 이동',),
+              const SizedBox(height: 30,),
+              const Text("학습 수정 및 삭제",style: TextStyle(fontSize: 17)),
+              const SizedBox(height: small_gap,),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/select_degree_page');
+                  },
+                  child: const Text("단어 수정 및 삭제 페이지로 이동")),
+              const SizedBox(height: small_gap,),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/select_degree_page');
+                  },
+                  child: const Text('문법 수정 및 삭제 페이지로 이동')),
+              const SizedBox(height: small_gap,),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/select_degree_page');
+                  },
+                  child: const Text('독해 수정 및 삭제 페이지로 이동')),
+              const SizedBox(height: small_gap,),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/select_degree_page');
+                  },
+                  child: const Text('논리 수정 및 삭제 페이지로 이동')),
+              const SizedBox(height: small_gap,),
+              TextButton(
+                  onPressed: () {
+                    logout();
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainPage() ));
+                  },
+                  child: const Text('로그 아웃')),
             ],
           ),
         ),
