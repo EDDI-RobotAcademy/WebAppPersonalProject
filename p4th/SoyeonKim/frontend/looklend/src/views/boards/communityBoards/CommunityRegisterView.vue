14c51fbae3f5ae48d56fc7f815f3935d0536e873
soyeonkim96 <shimmer326@gmail.com>
2022-11-22 16:19:31 +0900

    feat: [김소연] 로그인한 사용자가 커뮤니티에 게시글을 등록하기 위한 게시글 작성UI[LOOKLEND11]

---
+++ p4th/SoyeonKim/frontend/looklend/src/views/boards/communityBoards/CommunityRegisterView.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav>
+    <header-view-practice/>
+
+    <br/><br/><br/><br/><br/>
+
+    <v-row>
+      <community-register @submit="onSubmit"/>
+    </v-row>
+
+    <br/>
+    <br/>
+    <br/>
+
+    <FooterForm/>
+  </nav>
+</template>
+
+<script>
+
+import HeaderViewPractice from "@/views/main/HeaderViewPractice";
+import CommunityRegister from "@/components/boards/communityBoards/CommunityRegister";
+import {mapActions} from "vuex";
+import FooterForm from "@/components/common/FooterForm";
+
+export default {
+  name: "CommunityRegisterView",
+  components: {FooterForm, HeaderViewPractice, CommunityRegister},
+  methods: {
+    ...mapActions([
+      'requestRegisterCommunityToSpring'
+    ]),
+    async onSubmit (payload) {
+      await this.requestRegisterCommunityToSpring(payload)
+      await this.$router.push({
+        name: 'CommunityListView'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*.header{
+  margin-top:5%;
+  margin-bottom: 6%;
+  width:100%;
+}*/
+</style>
