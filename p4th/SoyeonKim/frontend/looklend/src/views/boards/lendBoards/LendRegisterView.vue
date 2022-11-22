cf42f01666ba50a6ba8f067ad4d174189ec858ea
soyeonkim96 <shimmer326@gmail.com>
2022-11-22 16:33:29 +0900

    feat: [김소연] 로그인한 사용자가 빌려주기에 게시글을 등록하기 위한 게시글 작성UI[LOOKLEND18]

---
+++ p4th/SoyeonKim/frontend/looklend/src/views/boards/lendBoards/LendRegisterView.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <header-view-practice/>
+
+    <br/><br/><br/><br/><br/>
+
+    <v-row justify="center">
+      <lend-register @submit="onSubmit"/>
+    </v-row>
+
+    <br/><br/><br/>
+
+    <FooterForm/>
+  </nav>
+</template>
+
+<script>
+import LendRegister from "@/components/boards/lendBoards/LendRegister";
+import {mapActions} from "vuex";
+import FooterForm from "@/components/common/FooterForm";
+import HeaderViewPractice from "@/views/main/HeaderViewPractice";
+
+export default {
+  name: "LendRegisterView",
+  components: {
+    HeaderViewPractice,
+    FooterForm,
+    LendRegister
+  },
+  methods: {
+    ...mapActions([
+      'requestRegisterLendToSpring'
+    ]),
+    async onSubmit (payload) {
+      await this.requestRegisterLendToSpring(payload)
+      await this.$router.push({
+        name: 'LendListView'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
