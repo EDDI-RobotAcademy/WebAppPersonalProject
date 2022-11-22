dfb869922ce22dca3115b098c8aa92468049e02a
choijaemin <foertonw5@naver.com>
2022-11-22 23:53:27 +0900

    feat :[최재민] 관리자 아이디 확인을 위한 로직 추가[Learning-Helper2]

--- p4th/JaeminChoi/spring/LeaningHelper/src/main/java/kr/eddi/LeaningHelper/service/member/MemberService.java
+++ p4th/JaeminChoi/spring/LeaningHelper/src/main/java/kr/eddi/LeaningHelper/service/member/MemberService.java
@@ -3,13 +3,16 @@ package kr.eddi.LeaningHelper.service.member;
 import kr.eddi.LeaningHelper.request.member.MemberSignUpRequest;
 import kr.eddi.LeaningHelper.request.member.MemberSignInRequest;
 
+import java.util.Map;
+
 public interface MemberService {
     public Boolean memberSignUp(MemberSignUpRequest memberSignUpRequest);
 
     public Integer emailValidation(String email);
 
-    public Boolean nickNameValidation(String nickName);
+    public Integer nickNameValidation(String nickName);
 
-    public String signIn(MemberSignInRequest memberSignInRequest);
+    public Map<String, String> signIn(MemberSignInRequest memberSignInRequest);
+
 
 }
