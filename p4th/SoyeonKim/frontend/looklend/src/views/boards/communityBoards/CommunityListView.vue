a9a7ce3451c71d7ddb53fdb798ef9c7ae1ede148
soyeonkim96 <shimmer326@gmail.com>
2022-11-22 16:12:10 +0900

    모든 사용자가 커뮤니티 게시판을 눌렀을 때의 리스트 UI[LOOKLEND10]

---
+++ p4th/SoyeonKim/frontend/looklend/src/views/boards/communityBoards/CommunityListView.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <header-view-practice/>
+
+    <br/>
+
+    <v-row justify="center">
+      <community-list :communityBoards="communityBoards"/>
+    </v-row>
+
+    <br/>
+    <br/>
+    <br/>
+
+
+    <FooterForm/>
+  </nav>
+</template>
+
+<script>
+import CommunityList from "@/components/boards/communityBoards/CommunityList";
+import HeaderViewPractice from "@/views/main/HeaderViewPractice";
+import {mapActions, mapState} from "vuex";
+import FooterForm from "@/components/common/FooterForm";
+export default {
+  name: "CommunityListView",
+  components: {
+    FooterForm,
+    CommunityList,
+    HeaderViewPractice,
+  },
+  computed: {
+    ...mapState(['communityBoards'])
+  },
+  mounted () {
+    this.requestCommunityListFromSpring()
+  },
+  methods: {
+    ...mapActions(['requestCommunityListFromSpring'])
+  }
+}
+</script>
+<style scoped>
+
+</style>
