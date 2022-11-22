3d933f2373b576d2a5df3c63dc90c20b9076c437
soyeonkim96 <shimmer326@gmail.com>
2022-11-22 16:34:43 +0900

    feat: [김소연] 로그인한 사용자가 커뮤니티 게시판에 글을 등록하기 위한 시스템 entity, repository [LOOKLEND19]

--- p4th/SoyeonKim/backend/look_lend_backend/src/main/java/com/example/look_lend_backend/entity/board/LendBoard.java
+++ p4th/SoyeonKim/backend/look_lend_backend/src/main/java/com/example/look_lend_backend/entity/board/LendBoard.java
@@ -36,3 +36,4 @@ public class LendBoard {
     @UpdateTimestamp
     private Date updDate;
 }
+
--- p4th/SoyeonKim/backend/look_lend_backend/src/main/java/com/example/look_lend_backend/repository/board/LendBoardRepository.java
+++ p4th/SoyeonKim/backend/look_lend_backend/src/main/java/com/example/look_lend_backend/repository/board/LendBoardRepository.java
@@ -7,3 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LendBoardRepository extends JpaRepository<LendBoard, Long> {
 
 }
+
