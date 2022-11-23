2c4e7591dddb6b030103ff4e56b94f6d0d5fb501
Hanchaeseon <mdmodina34@gmail.com>
2022-11-23 21:54:02 +0900

    feat. [한채선] 메인 페이지에 커뮤니티 최신 게시글 추가 [READUS-1]

---
+++ p4th/ChaeseonHan/readus_frontend/src/components/main/mainBoardList.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-card elevation="0" class="pa-2">
+      <common-board-list
+          :board-name="boardName"
+          :board-list="this.$store.state.communityMainBoardList"/>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import CommonBoardList from "@/components/common/CommonBoardList";
+import {mapActions} from "vuex";
+export default {
+  name: "mainBoardList",
+  components: {CommonBoardList},
+  data() {
+    return{
+      boardName: "〉 최신 게시글"
+    }
+  },
+  methods: {
+    ...mapActions(['requestAllCommunityBoardListTOSpring'])
+  },
+  mounted() {
+    this.requestAllCommunityBoardListTOSpring()
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
