f1daadbb0cd6aea725edd4a4ee96a48825ff107f
parkseonho1 <preferr1@naver.com>
2022-11-24 14:58:18 +0900

    init: [박선호] 코드 외 Spring 프로젝트 구성 파일 커밋

--- p4th/SeonhoPark/spring/look_style/src/main/java/personal_project/look_style/LookStyleApplication.java
+++ p4th/SeonhoPark/spring/look_style/src/main/java/personal_project/look_style/LookStyleApplication.java
@@ -3,8 +3,18 @@ package personal_project.look_style;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class LookStyleApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재시각 : " + new Date());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LookStyleApplication.class, args);
